d19579ab563d89fc8c2d00383e1166421dce24d4
steventian1101 <steventian1101@gmail.com>
2022-06-20 15:48:18 -0300

    today's deals

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,9 @@
 <x-app-layout>
+    <div class="row justify-content-center">
+        <h2 class="col-md-3 text-center">
+            Today's Deals
+            <hr>
+        </h2>
+    </div>
     <x-products-display :products="$products"/>
 </x-app-layout>
