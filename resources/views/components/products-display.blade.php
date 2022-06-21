b943f80d213f0c8e6ad9c7aea2dfe864ba144dc4
steventian1101 <steventian1101@gmail.com>
2022-06-21 06:18:33 +0000

    Update products-display.blade.php

--- resources/views/components/products-display.blade.php
+++ resources/views/components/products-display.blade.php
@@ -1,14 +1,21 @@
 @foreach ($products->chunk(4) as $products_chunk)
-    <div class="row justify-content-around mb-4">
+    <div class="row row-cols-xxl-6 row-cols-xl-6 row-cols-lg-4 row-cols-md-4 row-cols-2">
         @foreach ($products_chunk as $product)
-            <div class="product-block col-12 col-sm-6 col-md-2 border border-secondary border-5">
-                <a href="{{route('products.show', $product->id)}}" class="text-decoration-none d-inline-block link-dark h-100 w-100">
-                    <img src="{{asset($product->images->first()->path)}}" class="img-fluid d-block mx-auto mt-3" alt="{{$product->name}}">
-                    <div class="text-center mt-3">
-                        {{$product->name}}
+        <div class="col">
+            <a class="card hov-shadow-sm mt-1 mb-2 has-transition bg-white p-2" href="{{route('products.show', $product->id)}}">
+                <div class="w-100 mb-2 pb-3 border-bottom">
+                    <img class="rounded-sm w-100" src="{{asset($product->images->first()->path)}}" alt="{{$product->name}}">
+                </div>
+                <div class="text-left px-2">
+                    <div class="fs-15">
+                        <span class="fw-700 text-primary">${{$product->price}}</span>
                     </div>
-                </a>
-            </div>
+                    <h3 class="fw-600 fs-13 text-truncate-2 lh-1-4 mb-0 h-35px text-black">
+                        {{$product->name}}
+                    </h3>
+                </div>
+            </a>
+        </div>
         @endforeach
     </div>
 @endforeach
