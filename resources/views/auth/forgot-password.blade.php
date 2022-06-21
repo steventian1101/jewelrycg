64ba13c6fe8692c6e26b4a5ba4a842501844ced5
steventian1101 <steventian1101@gmail.com>
2022-06-21 15:54:56 +0000

    Update forgot-password.blade.php

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -27,7 +27,7 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <x-button>
+                <x-button class="btn-primary">
                     {{ __('Email Password Reset Link') }}
                 </x-button>
             </div>
