7b37d278b410e5a4beed9ef79a9b4821b9d0dadb
steventian1101 <steventian1101@gmail.com>
2022-06-21 05:11:57 +0000

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,13 +1,16 @@
-<header class="bg-light">
-    <div class="container d-flex align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-        <span class="d-flex align-items-center col-md-1 mb-2 mb-md-0">
-            <a href="{{ route('index') }}">
+<header class="navbar navbar-expand-lg py-3">
+    <nav class="container">
+        <a class="navbar-brand col-auto" href="{{ route('index') }}">
+                <!--
                 <img src="{{ asset('img/logo.png') }}" width="50" height="50" alt="logo">
-            </a>
-        </span>
-    
-        <div class="col-12 col-md-7 mb-2 mb-md-0 row">
-            <form method="get" action="{{route('products.search')}}" class="col-10 col-md-5 mb-2 justify-content-center mb-md-0 input-group">
+                <img class="logo" src="https://districtgurus.com/public/uploads/all/SC008HOLHmfOeB8E3SxNDONHI7nad1YJcmSl0ds9.png" data-src="https://districtgurus.com/public/uploads/all/SC008HOLHmfOeB8E3SxNDONHI7nad1YJcmSl0ds9.png" alt="District Gurus">-->
+                #JewelryCG
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <form method="get" action="{{route('products.search')}}" class="d-flex col-xl-6 col-lg-4 input-group">
     
                 <div class="input-group-prepend">
                     <select name="category" class="form-select text-small text-capitalize">
@@ -18,58 +21,71 @@
                     </select>
                 </div>
     
-                <input name="q" type="search" class="form-control" aria-label="Search">
+                <input name="q" type="search" class="form-control me-2" placeholder="Search" aria-label="Search">
             </form>
-    
-        </div>
 
-        <div class="col-2 text-center">
-            <a href="{{route('cart.index')}}" class="text-decoration-none">
-                <img src="{{asset('img/cart.png')}}" alt="cart" width="50" height="50" class="img-fluid bg-light">
-                <?php
-                    if(Cart::instance('default')->content()->count() == 0
-                        && auth()->check()
-                    )
-                    {
-                        Cart::merge(auth()->id());
-                    }
-                ?>
-                @if ($cart_items = Cart::content()->count())
-                    <span class="rounded-pill pill badge bg-primary text-light">
-                        {{$cart_items}}
-                    </span>
-                @endif
-            </a>
-        </div>
-    
-        @auth
-            <div class="col-md-2 dropdown text-end" align="end">
-                <a href="#" class="text-decoration-none link-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                    {{ Auth::user()->name }}
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end text-small">
-                    <li>
-                        <a class="dropdown-item" href="{{route('user.index', auth()->user()->id)}}">
-                            My Info
-                        </a>
-                    </li>
-                    <li><a class="dropdown-item" href="{{route('orders.index')}}">{{ auth()->user()->is_admin ? 'All Orders' : 'My Orders' }}</a></li>
-                    <li><a class="dropdown-item" href="{{route('cart.wishlist')}}">My Wishlist</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    @if (auth()->user()->is_admin)
-                        <li><a class="dropdown-item" href="{{route('products.create')}}">Add New Product</a></li>
+            <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown menu-area">
+                    <a class="nav-link active" href="{{ route('index') }}">Home</a>
+                </li>
+                <li class="nav-item dropdown menu-area">
+                    <a class="nav-link active" href="/">3D Models</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link active dropdown-toggle" aria-current="page" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" href="#">Learn</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Blog</a></li>
+                        <li><a class="dropdown-item" href="#">Categories</a></li>
+                        <li><a class="dropdown-item" href="#">Tags</a></li>
+                        <li><a class="dropdown-item" href="#">Comments</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown menu-area">
+                    <a class="nav-link" href="/">Cart</a>
+                    <a href="{{route('cart.index')}}" class="nav-link">
+                        <i class="bi bi-cart2"></i>
+                        <?php
+                            if(Cart::instance('default')->content()->count() == 0
+                                && auth()->check()
+                            )
+                            {
+                                Cart::merge(auth()->id());
+                            }
+                        ?>
+                        @if ($cart_items = Cart::content()->count())
+                            <span class="rounded-pill pill badge bg-primary text-light">
+                                {{$cart_items}}
+                            </span>
+                        @endif
+                    </a>
+                </li>
+
+                @auth
+                <li class="nav-item dropdown">
+                    <a class="nav-link active dropdown-toggle" aria-current="page" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" href="#">{{ Auth::user()->name }}</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="/dashboard">Dashboard</a></li>
+                        <li><a class="dropdown-item" href="{{route('user.index', auth()->user()->id)}}">My Info</a></li>
+                        <li><a class="dropdown-item" href="{{route('orders.index')}}">{{ auth()->user()->is_admin ? 'All Orders' : 'My Orders' }}</a></li>
+                        <li><a class="dropdown-item" href="{{route('cart.wishlist')}}">My Wishlist</a></li>
                         <li><hr class="dropdown-divider"></li>
-                    @endif
-                    <li><a class="dropdown-item" href="{{ route('logout') }}">Logout</a></li>
-                </ul>
-            </div>
-        @else
-            <div class="col-md-2 text-end">
-                <a href="{{ route('login') }}" class="btn btn-outline-primary me-2">Login</a>
-                <a href="{{ route('register') }}" class="btn btn-primary">Sign-up</a>
-            </div>
-        @endauth
-    
-    
-    </div>
+                        @if (auth()->user()->is_admin)
+                            <li><a class="dropdown-item" href="{{route('products.create')}}">Add New Product</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                        @endif
+                        <li><a class="dropdown-item" href="{{ route('logout') }}">Logout</a></li>
+                    </ul>
+                </li>
+                @else
+                <li class="nav-item ml-1">
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>  
+                </li>
+                <li class="nav-item ml-1">
+                    <a class="nav-link" href="{{ route('register') }}">Register</a>  
+                </li>
+                @endauth
+
+            </ul>
+        </div>
+    </nav>
 </header>
