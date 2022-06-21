424b70e4573f3f4ee03d781bfad5f953b8ed8222
steventian1101 <steventian1101@gmail.com>
2022-06-21 15:07:09 +0000

    Update auth-card.blade.php

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,6 +1,7 @@
-<div class="p-10">
-    <div>
-        {{ $logo }}
+<div class="guest-content p-10">
+    <div class="logo-text">
+        <!--{{ $logo }}-->
+        #JewelryCG
     </div>
 
     <div class="col-6 ml-auto mr-auto card rounded">
