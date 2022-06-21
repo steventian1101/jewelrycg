a8b6cc48a58e77bec7be52cfde6aca6fe7fb06cd
steventian1101 <steventian1101@gmail.com>
2022-06-21 22:25:59 +0000

    Update show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -160,8 +160,8 @@
                             <form action="{{route('cart.store')}}" method="post" class="my-3">
                                 @csrf
                                 <input type="hidden" name="id_product" value="{{$product->id}}">
-                                <button type="submit" class="btn btn-primary" {{ $product->qty < 1 ? 'disabled' : null }}>Add to Cart</button>
-                                <button type="submit" formaction="{{route('cart.buy.now')}}" class="btn btn-warning" {{ $product->qty < 1 ? 'disabled' : null }}>Buy Now</button>
+                                <button type="submit" class="btn btn-primary shadow-md" {{ $product->qty < 1 ? 'disabled' : null }}>Add to Cart</button>
+                                <button type="submit" formaction="{{route('cart.buy.now')}}" class="btn btn-success shadow-md" {{ $product->qty < 1 ? 'disabled' : null }}>Buy Now</button>
                             </form>        
 
                         </div><!--End .bg-white product card-->
