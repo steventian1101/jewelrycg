fda794f1d6d2d306e419c3d61bd6efa4f80e46da
steventian1101 <steventian1101@gmail.com>
2022-06-21 06:06:31 +0000

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,51 +1,51 @@
-<section class="hero-content-container py-8">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12">
-                <h4 class="fs-20 text-black mb-4">The world's preferred source for Jewelry CG content</h4>
-				<h1 class="text-black font-weight-bold mb-4">Explore our vast collections of 3D models</h1>
-            </div>
-			<div class="col-4 col-lg-2">
-                <div class="card p-3">
-                    <i class="fa-brands fa-twitter"></i>
-                    <div class="title text-black">Pendant</div>
-                </div>
-            </div>
-            <div class="col-4 col-lg-2">
-                <div class="card p-3">
-                    <i class="fa-brands fa-twitter"></i>
-                    <div class="title text-black">Pendant</div>
-                </div>
-            </div>
-            <div class="col-4 col-lg-2">
-                <div class="card p-3">
-                    <i class="fa-brands fa-twitter"></i>
-                    <div class="title text-black">Pendant</div>
-                </div>
-            </div>
-            <div class="col-4 col-lg-2">
-                <div class="card p-3">
-                    <i class="fa-brands fa-twitter"></i>
-                    <div class="title text-black">Pendant</div>
-                </div>
-            </div>
-            <div class="col-4 col-lg-2">
-                <div class="card p-3">
-                    <i class="fa-brands fa-twitter"></i>
-                    <div class="title text-black">Pendant</div>
-                </div>
-            </div>
-            <div class="col-4 col-lg-2">
-                <div class="card p-3">
-                    <i class="fa-brands fa-twitter"></i>
-                    <div class="title text-black">Pendant</div>
-                </div>
-            </div>
-		</div>
-	</div>
-</section>
-
 <x-app-layout>
+    <section class="hero-content-container py-8">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h4 class="fs-20 text-black mb-4">The world's preferred source for Jewelry CG content</h4>
+                    <h1 class="text-black font-weight-bold mb-4">Explore our vast collections of 3D models</h1>
+                </div>
+                <div class="col-4 col-lg-2">
+                    <div class="card p-3">
+                        <i class="fa-brands fa-twitter"></i>
+                        <div class="title text-black">Pendant</div>
+                    </div>
+                </div>
+                <div class="col-4 col-lg-2">
+                    <div class="card p-3">
+                        <i class="fa-brands fa-twitter"></i>
+                        <div class="title text-black">Pendant</div>
+                    </div>
+                </div>
+                <div class="col-4 col-lg-2">
+                    <div class="card p-3">
+                        <i class="fa-brands fa-twitter"></i>
+                        <div class="title text-black">Pendant</div>
+                    </div>
+                </div>
+                <div class="col-4 col-lg-2">
+                    <div class="card p-3">
+                        <i class="fa-brands fa-twitter"></i>
+                        <div class="title text-black">Pendant</div>
+                    </div>
+                </div>
+                <div class="col-4 col-lg-2">
+                    <div class="card p-3">
+                        <i class="fa-brands fa-twitter"></i>
+                        <div class="title text-black">Pendant</div>
+                    </div>
+                </div>
+                <div class="col-4 col-lg-2">
+                    <div class="card p-3">
+                        <i class="fa-brands fa-twitter"></i>
+                        <div class="title text-black">Pendant</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
     <div class="row justify-content-center">
         <h2 class="col-md-3 text-center">
             Today's Deals
