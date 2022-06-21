95cda99a7ce21ed19e176223fe3d70fac02cd039
steventian1101 <steventian1101@gmail.com>
2022-06-21 14:52:16 +0000

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{$pageTitle}}</title>
+        <title>Jewelry CG</title>
         <link rel="shortcut icon" type="image/png" href="{{ asset('img/favicon.png') }}"/>
 
         <!-- Google Fonts -->
