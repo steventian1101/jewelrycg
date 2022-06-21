ba75424ae9ab3318e9fa468018e039dc4a2f6e96
steventian1101 <steventian1101@gmail.com>
2022-06-21 16:54:47 +0000

    Update verify-email.blade.php

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -21,7 +21,7 @@
                 @csrf
 
                 <div>
-                    <x-button>
+                    <x-button class="btn-primary">
                         {{ __('Resend Verification Email') }}
                     </x-button>
                 </div>
