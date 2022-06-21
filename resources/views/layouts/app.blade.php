2a77384ff014a5a54c06128e81af5e228803f46f
steventian1101 <steventian1101@gmail.com>
2022-06-21 04:52:26 +0000

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,15 +24,17 @@
         <script src="{{asset('js/func_util.js')}}" defer></script>
     </head>
     <body>
-        <div>
-            <!-- Page Heading -->
-            <x-header/>
 
-            <!-- Page Content -->
-            <main class="container">
-                {{ $slot }}
-            </main>
-        </div>
+        <!-- Page Heading -->
+        <x-header/>
+
+        <!-- Page Content -->
+        <main class="container">
+            {{ $slot }}
+        </main>
+
+        <!-- Page Footer -->
+        <x-footer/>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     </body>
