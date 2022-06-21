39b0df9e0ca3ac581e9a36f4e4ca1c39c5ce27da
steventian1101 <steventian1101@gmail.com>
2022-06-21 15:59:50 +0000

    Update auth-card.blade.php

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,10 +1,10 @@
 <div class="guest-content pt-10">
     <div class="col-12 col-lg-6 ml-auto mr-auto">
-    <!--
-        <div class="logo-container logo-text text-center fs-20 mb-4 fw-700">
-            {{ $logo }}
-        </div>
--->
+
+        <!--<div class="logo-container logo-text text-center fs-20 mb-4 fw-700">-->
+           <!-- {{ $logo }}-->
+        <!--</div>-->
+
         <div class="p-3 card rounded">
             {{ $slot }}
         </div>
