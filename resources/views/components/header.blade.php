f71b51be9e5928ea97d0fadb12acf0f3fd5bc4ba
steventian1101 <steventian1101@gmail.com>
2022-06-21 23:46:15 +0000

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -12,7 +12,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <form method="get" action="{{route('products.search')}}" class="d-flex col-xl-6 col-lg-4">
     
-                <div class="input-group-prepend">
+                <div class="mr-2">
                     <select name="category" class="form-control text-small text-capitalize">
                         <option>All</option>
                         @foreach (\App\Models\Product::$category_list as $category)
