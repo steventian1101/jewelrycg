9b3921d9d9f830a942fa6f4f347ca6fb499fe5d5
steventian1101 <steventian1101@gmail.com>
2022-06-21 23:35:04 +0000

    Update search.blade.php

--- resources/views/products/search.blade.php
+++ resources/views/products/search.blade.php
@@ -1,3 +1,10 @@
 <x-app-layout page-title="Searching for {{request()->q ?? request()->category}}">
-    <x-products-display :products="$products"/>
+    <section class="product_detail_single py-4">
+        <div class="container">
+            <div class="search-page-title mb-2">
+                <h1 class="text-black fw-700">Searching for {{request()->q ?? request()->category}}</h1>
+            </div>
+            <x-products-display :products="$products"/>
+        </div>
+    </section>
 </x-app-layout>
