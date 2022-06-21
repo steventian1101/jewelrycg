ca02c1e51c2807400db4b4cc6002be0fab5d1d4c
steventian1101 <steventian1101@gmail.com>
2022-06-21 15:53:52 +0000

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,7 +47,7 @@
                     </a>
                 @endif
 
-                <x-button class="ml-3">
+                <x-button class="ml-3 btn-primary">
                     {{ __('Log in') }}
                 </x-button>
             </div>
