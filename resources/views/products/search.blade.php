fb358893c624561a8f6cdf533f9123827cd1c180
steventian1101 <steventian1101@gmail.com>
2022-06-21 23:31:58 +0000

    Update search.blade.php

--- resources/views/products/search.blade.php
+++ resources/views/products/search.blade.php
@@ -1,6 +1,3 @@
 <x-app-layout page-title="Searching for {{request()->q ?? request()->category}}">
     <x-products-display :products="$products"/>
-    <div class="mt-3 text-center">
-        {{ $products->links() }}
-    </div>
 </x-app-layout>
