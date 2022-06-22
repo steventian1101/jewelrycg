33cb57163503cdbc1b4c72e16b8ee210c419b61e
steventian1101 <steventian1101@gmail.com>
2022-06-22 00:33:17 +0000

    Update auth-card.blade.php

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,4 +1,4 @@
-<div class="guest-content pt-10">
+<div class="guest-content py-5">
     <div class="col-xxl-4 col-xl-5 col-lg-6 col-md-8 mx-auto">
 
         <!--<div class="logo-container logo-text text-center fs-20 mb-4 fw-700">-->
