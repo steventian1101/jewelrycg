6e0a079e7794af539345ec096ca7fad97a8be075
steventian1101 <steventian1101@gmail.com>
2022-06-22 00:02:09 +0000

    Update AppLayout.php

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -6,7 +6,7 @@ use Illuminate\View\Component;
 
 class AppLayout extends Component
 {
-    public function __construct(public string $pageTitle = 'Shopp')
+    public function __construct(public string $pageTitle = 'Jewelry CG')
     {
     }
     /**
