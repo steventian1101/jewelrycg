6c50cde501c8bb7607d8ebd8a7b33e80d9f6b74d
steventian1101 <steventian1101@gmail.com>
2022-06-26 02:39:35 +0200

    update seller

--- app/Http/Controllers/Backend/VendorsController.php
+++ app/Http/Controllers/Backend/VendorsController.php
@@ -45,7 +45,7 @@ class VendorsController extends Controller
         ->addColumn('action', function($row){
 
                 $btn = '<a href="javascript:void(0)" class="edit btn btn-success btn-sm">Products</a>';
-               $btn = $btn . '<a href="'.route('backend.products.edit', $row->id).'" class="edit btn btn-primary btn-sm">Edit</a>';
+               $btn = $btn . '<a href="'.route('backend.sellers.edit', $row->id).'" class="edit btn btn-primary btn-sm">Edit</a>';
                $btn = $btn.'<a href="javascript:void(0)" class="edit btn btn-danger btn-sm">Delete</a>';
                
 
@@ -108,7 +108,10 @@ class VendorsController extends Controller
      */
     public function edit($id)
     {
-        //
+        return view('backend.dashboard.sellers.edit',[
+            'seller' => User::findOrFail($id)
+        ]);
+        
     }
 
     /**
@@ -120,7 +123,22 @@ class VendorsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,'.$user->id],
+        ]);
+        
+        $user->name = $request->name;
+        $user->email = $request->email;
+        if($request->password)
+        {
+            $user->password = Hash::make($request->password);
+        }
+        $user->update();
+
+        return redirect()->route('backend.sellers.list');
+
     }
 
     /**
--- resources/views/backend/dashboard/sellers/edit.blade.php
+++ resources/views/backend/dashboard/sellers/edit.blade.php
@@ -10,19 +10,20 @@
                             <div class="card-body">
                                 @include('includes.validation-form')
 
-                                <form method="POST" action="{{ route('backend.sellers.store') }}">
+                                <form method="POST" action="{{ route('backend.sellers.update', $seller->id) }}">
                                     @csrf
+                                    @method('PUT')
                                     <div class="row">
                                         <!-- Name -->
                                         <div class="col-md-6 mb-2">
                                             <label for="name">Name:</label>
-                                            <input type="text" name="name" id="name" class="form-control">
+                                            <input type="text" name="name" value='{{ $seller->name }}' id="name" class="form-control">
                                         </div>
 
                                         <!-- Email Address -->
                                         <div class="col-md-6 mb-2">
                                             <label for="email">Email:</label>
-                                            <input type="text" name="email" id="email" class="form-control">
+                                            <input type="text" name="email" value='{{ $seller->email }}' id="email" class="form-control">
                                         </div>
 
                                     </div>
@@ -32,7 +33,7 @@
                                         <x-label for="password" :value="__('Password')" />
 
                                         <x-input id="password" class="block mt-1 w-full" type="password" name="password"
-                                            required autocomplete="new-password" />
+                                             autocomplete="new-password" />
                                     </div>
 
                                     <!-- Confirm Password -->
@@ -40,12 +41,12 @@
                                         <x-label for="password_confirmation" :value="__('Confirm Password')" />
 
                                         <x-input id="password_confirmation" class="block mt-1 w-full" type="password"
-                                            name="password_confirmation" required />
+                                            name="password_confirmation"  />
                                     </div>
                                     </div>
                                     <div class="col-md-12 text-center">
                                         <button type="submit"
-                                            class="btn btn-lg btn-outline-success">Add seller</button>
+                                            class="btn btn-lg btn-outline-success">Update seller</button>
                                     </div>
                             </div>
                         </div>
