c655115916f9b8711598272e8f462d520ed1028b
steventian1101 <steventian1101@gmail.com>
2022-06-26 20:45:32 +0000

    Update nav.blade.php

--- resources/views/backend/dashboard/layouts/footer/nav.blade.php
+++ resources/views/backend/dashboard/layouts/footer/nav.blade.php
@@ -2,7 +2,7 @@
 <div class="footer">
       <div class="row justify-content-between align-items-center">
         <div class="col">
-          <p class="fs-6 mb-0">© Front. <span class="d-none d-sm-inline-block">2022 G.G.M & Co.</span></p>
+          <p class="fs-6 mb-0">© 2022 G.G.M & Co.</p>
         </div>
         <!-- End Col -->
 
