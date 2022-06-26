c13880a12d998db3700e7b58bfc37e1b5f062d09
steventian1101 <steventian1101@gmail.com>
2022-06-26 14:53:43 +0000

    Update list.blade.php

--- resources/views/backend/dashboard/blog/posts/list.blade.php
+++ resources/views/backend/dashboard/blog/posts/list.blade.php
@@ -1,4 +1,4 @@
-@extends('backend.dashboard.layouts.app', ['activePage' => 'posts', 'title' => 'Light Bootstrap Dashboard Laravel by Creative Tim & UPDIVISION', 'navName' => 'Table List', 'activeButton' => 'blog'])
+@extends('backend.dashboard.layouts.app', ['activePage' => 'posts', 'title' => 'All Post', 'navName' => 'Table List', 'activeButton' => 'blog'])
 
 @section('content')
     <div class="content">
