3fad76b603c22d83e5fc8e6ed05aff8cc0dc9e92
steventian1101 <steventian1101@gmail.com>
2022-06-26 14:52:34 +0000

    Update sidebar.blade.php

--- resources/views/backend/dashboard/layouts/navbars/sidebar.blade.php
+++ resources/views/backend/dashboard/layouts/navbars/sidebar.blade.php
@@ -66,7 +66,7 @@
                   <span class="nav-link-title">{{ __("Post") }}</span>
                 </a>
 
-                <div id="navbarVerticalMenuAllPostMenu" class="nav-collapse collapse " data-bs-parent="#navbarVerticalMenuPagesMenu">
+                <div id="navbarVerticalMenuAllPostMenu" class="nav-collapse collapse @if($activePage == 'posts') show @endif" data-bs-parent="#navbarVerticalMenuPagesMenu">
                   <a class="nav-link " href="{{ route('backend.posts.list') }}">{{ __("All Post") }}</a>
                   <a class="nav-link " href="{{ route('backend.posts.create') }}">{{ __("Add New") }}</a>
                   <a class="nav-link " href="{{ route('backend.categories.list') }}">{{ __("Categories") }}</a>
@@ -82,7 +82,7 @@
                   <span class="nav-link-title">{{ __("Pages") }}</span>
                 </a>
 
-                <div id="navbarVerticalMenuAllPagesMenu" class="nav-collapse collapse " data-bs-parent="#navbarVerticalMenuPagesMenu">
+                <div id="navbarVerticalMenuAllPagesMenu" class="nav-collapse collapse @if($activePage == 'pages') show @endif" data-bs-parent="#navbarVerticalMenuPagesMenu">
                   <a class="nav-link " href="#">{{ __("All Pages") }}</a>
                   <a class="nav-link " href="#">{{ __("Add New") }}</a>
                 </div>
@@ -106,7 +106,7 @@
                   <span class="nav-link-title">{{ __("Products") }}</span>
                 </a>
 
-                <div id="navbarVerticalMenuAllProductsMenu" class="nav-collapse collapse " data-bs-parent="#navbarVerticalMenuPagesMenu">
+                <div id="navbarVerticalMenuAllProductsMenu" class="nav-collapse collapse @if($activePage == 'products') show @endif" data-bs-parent="#navbarVerticalMenuPagesMenu">
                   <a class="nav-link " href="{{ route('backend.products.list') }}">{{ __("All Products") }}</a>
                   <a class="nav-link " href="{{ route('backend.products.create') }}">{{ __("Add New") }}</a>
                   <a class="nav-link " href="#">{{ __("Attributes") }}</a>
@@ -123,9 +123,9 @@
                   <span class="nav-link-title">{{ __("Orders") }}</span>
                 </a>
 
-                <div id="navbarVerticalMenuAllOrdersMenu" class="nav-collapse collapse " data-bs-parent="#navbarVerticalMenuPagesMenu">
-                  <a class="nav-link " href="{{ route('backend.products.list') }}">{{ __("All Orders") }}</a>
-                  <a class="nav-link " href="{{ route('backend.products.create') }}">{{ __("Pending") }} <span class="badge bg-primary rounded-pill ms-1">5</span></a>
+                <div id="navbarVerticalMenuAllOrdersMenu" class="nav-collapse collapse @if($activePage == 'orders') show @endif" data-bs-parent="#navbarVerticalMenuPagesMenu">
+                  <a class="nav-link " href="#">{{ __("All Orders") }}</a>
+                  <a class="nav-link " href="#">{{ __("Pending") }} <span class="badge bg-primary rounded-pill ms-1">5</span></a>
                 </div>
               </div>
               <!-- End Collapse -->
