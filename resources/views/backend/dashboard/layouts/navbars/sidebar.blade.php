48426e86602df67614192a93a5baf9514c0a5d73
steventian1101 <steventian1101@gmail.com>
2022-06-26 13:54:57 +0000

    Update sidebar.blade.php

--- resources/views/backend/dashboard/layouts/navbars/sidebar.blade.php
+++ resources/views/backend/dashboard/layouts/navbars/sidebar.blade.php
@@ -5,10 +5,11 @@
         <!-- Logo -->
 
         <a class="navbar-brand" href="../index.html" aria-label="Front">
-          <img class="navbar-brand-logo" src="../assets/svg/logos/logo.svg" alt="Logo" data-hs-theme-appearance="default">
-          <img class="navbar-brand-logo" src="../assets/svg/logos-light/logo.svg" alt="Logo" data-hs-theme-appearance="dark">
-          <img class="navbar-brand-logo-mini" src="../assets/svg/logos/logo-short.svg" alt="Logo" data-hs-theme-appearance="default">
-          <img class="navbar-brand-logo-mini" src="../assets/svg/logos-light/logo-short.svg" alt="Logo" data-hs-theme-appearance="dark">
+          <img class="navbar-brand-logo" src="{{ asset('assets/svg/logos/logo.svg') }}" alt="Logo" data-hs-theme-appearance="default">
+          <img class="navbar-brand-logo" src="{{ asset('assets/svg/logos-light/logo.svg') }}" alt="Logo" data-hs-theme-appearance="dark">
+          <img class="navbar-brand-logo-mini" src="{{ asset('assets/svg/logos/logo-short.svg') }}" alt="Logo" data-hs-theme-appearance="default">
+          <img class="navbar-brand-logo-mini" src="{{ asset('assets/svg/logos-light/logo-short.svg') }}" alt="Logo" data-hs-theme-appearance="dark">
+
         </a>
 
         <!-- End Logo -->
