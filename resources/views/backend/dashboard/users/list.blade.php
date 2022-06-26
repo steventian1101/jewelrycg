a37c0b3a87278b160d1091f3edeb27217048c398
steventian1101 <steventian1101@gmail.com>
2022-06-26 14:44:50 +0000

    Update list.blade.php

--- resources/views/backend/dashboard/users/list.blade.php
+++ resources/views/backend/dashboard/users/list.blade.php
@@ -1,6 +1,32 @@
-@extends('backend.dashboard.layouts.app', ['activePage' => 'users', 'title' => 'Light Bootstrap Dashboard Laravel by Creative Tim & UPDIVISION', 'navName' => 'Table List', 'activeButton' => 'laravel'])
+@extends('backend.dashboard.layouts.app', ['activePage' => 'users', 'title' => 'All Users', 'navName' => 'Table List', 'activeButton' => 'laravel'])
 
 @section('content')
+<!-- Page Header -->
+<div class="page-header">
+    <div class="row align-items-end">
+        <div class="col-sm mb-2 mb-sm-0">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb breadcrumb-no-gutter">
+            <li class="breadcrumb-item"><a class="breadcrumb-link" href="javascript:;">Pages</a></li>
+            <li class="breadcrumb-item"><a class="breadcrumb-link" href="javascript:;">Users</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Overview</li>
+            </ol>
+        </nav>
+
+        <h1 class="page-header-title">Users</h1>
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-auto">
+        <a class="btn btn-primary" href="./users-add-user.html">
+            <i class="bi-person-plus-fill me-1"></i> Add user
+        </a>
+        </div>
+        <!-- End Col -->
+    </div>
+    <!-- End Row -->
+</div>
+<!-- End Page Header -->
     <div class="content">
         <div class="container-fluid">
             <div class="row">
