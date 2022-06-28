c7af92711b1931314df8f2285a74ea3db03e2157
steventian1101 <steventian1101@gmail.com>
2022-06-28 03:46:31 +0000

    Update list.blade.php

--- resources/views/backend/dashboard/blog/posts/list.blade.php
+++ resources/views/backend/dashboard/blog/posts/list.blade.php
@@ -1,4 +1,4 @@
-@extends('backend.dashboard.layouts.app', ['activePage' => 'posts', 'title' => 'All Post', 'navName' => 'Table List', 'activeButton' => 'blog'])
+@extends('backend.dashboard.layouts.app', ['activePage' => 'posts', 'title' => 'All Post', 'navName' => 'allpost', 'activeButton' => 'blog'])
 
 @section('content')
 <div class="page-header">
