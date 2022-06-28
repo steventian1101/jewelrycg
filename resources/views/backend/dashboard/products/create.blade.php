b390e9a7b73187c0f79761b7fb14d8d8dcdefe76
steventian1101 <steventian1101@gmail.com>
2022-06-28 13:32:28 +0000

    Update create.blade.php

--- resources/views/backend/dashboard/products/create.blade.php
+++ resources/views/backend/dashboard/products/create.blade.php
@@ -28,69 +28,70 @@
     <!-- End Row -->
 </div>
 
-<div class="row">
 <form action="{{ route('backend.products.store') }}" method="post" enctype="multipart/form-data">
     @csrf
-    <div class="col-lg-8 mb-3 mb-lg-0">
+    <div class="row">
+        <div class="col-lg-8 mb-3 mb-lg-0">
 
-            <div class="card col-md-12">
-                <div class="card-body row">
-                    @include('includes.validation-form')
-                    <div class="col-md-12 mb-2">
-                        <label for="name">Name:</label>
-                        <input type="text" name="name" id="name" class="form-control">
-                    </div>
-                    <div class="col-md-12 mb-2">
-                        <label for="desc">Description:</label>
-                        <textarea name="desc" id="desc" rows="3" class="form-control">
-                        </textarea>
-                    </div>
-                    <div class="col-md-6 mb-2">
-                        <label for="price">Price:</label>
-                        <input type="text" name="price" id="price" class="form-control" placeholder="80.00...">
-                    </div>
-                    <div class="col-md-6 mb-2">
-                        <label for="qty">Quantity in Stock:</label>
-                        <input type="number" name="qty" id="qty" class="form-control" min="0">
-                    </div>
-                    <div class="col-md-6 mb-2">
-                        <label for="category">Category:</label>
-                        <div class="col-md-12">
-                            <select style="width:100%" class="selectpicker" name="category" data-live-search="true">
-                                @foreach ($categories as $categorie)
-                                    <option value="{{$categorie->id}}" data-tokens="{{$categorie->category_name}}">{{$categorie->category_name}}</option>
-                                @endforeach
-                                
-                                </select>
+                <div class="card col-md-12">
+                    <div class="card-body row">
+                        @include('includes.validation-form')
+                        <div class="col-md-12 mb-2">
+                            <label for="name">Name:</label>
+                            <input type="text" name="name" id="name" class="form-control">
                         </div>
-                    </div>
-                    <div class="col-md-6 mb-2">
-                        <label for="name">Tags:</label>
-                        <select  name="tags[]" id="tags" value="" class="form-control select2"  multiple="multiple" style="width: 100%;">
-                            @foreach ($tags as $tag)
-                                <option value='{{ $tag->id }}'> {{ $tag->name }} </option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="col-md-12 mb-3">
-                        <label
-                            for="images">Images</label>
-                        <input type="file" name="images[]" id="images" class="form-control" multiple>
-                    </div>
-                    <div class="col-md-12 text-center">
-                        <button type="submit"
-                            class="btn btn-lg btn-outline-success">Add</button>
-                    </div>
+                        <div class="col-md-12 mb-2">
+                            <label for="desc">Description:</label>
+                            <textarea name="desc" id="desc" rows="3" class="form-control">
+                            </textarea>
+                        </div>
+                        <div class="col-md-6 mb-2">
+                            <label for="price">Price:</label>
+                            <input type="text" name="price" id="price" class="form-control" placeholder="80.00...">
+                        </div>
+                        <div class="col-md-6 mb-2">
+                            <label for="qty">Quantity in Stock:</label>
+                            <input type="number" name="qty" id="qty" class="form-control" min="0">
+                        </div>
+                        <div class="col-md-6 mb-2">
+                            <label for="category">Category:</label>
+                            <div class="col-md-12">
+                                <select style="width:100%" class="selectpicker" name="category" data-live-search="true">
+                                    @foreach ($categories as $categorie)
+                                        <option value="{{$categorie->id}}" data-tokens="{{$categorie->category_name}}">{{$categorie->category_name}}</option>
+                                    @endforeach
+                                    
+                                    </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-2">
+                            <label for="name">Tags:</label>
+                            <select  name="tags[]" id="tags" value="" class="form-control select2"  multiple="multiple" style="width: 100%;">
+                                @foreach ($tags as $tag)
+                                    <option value='{{ $tag->id }}'> {{ $tag->name }} </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <label
+                                for="images">Images</label>
+                            <input type="file" name="images[]" id="images" class="form-control" multiple>
+                        </div>
+                        <div class="col-md-12 text-center">
+                            <button type="submit"
+                                class="btn btn-lg btn-outline-success">Add</button>
+                        </div>
 
+                    </div>
                 </div>
-            </div>
- 
-    </div>
-    <div class="col-lg-4">
+    
+        </div>
+        <div class="col-lg-4">
 
+        </div>
     </div>
 </form>
-</div>
+
 
 @endsection
 
