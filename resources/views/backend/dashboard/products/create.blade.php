82b0000bb5eb99caa0ad08a7e2f2814192544406
steventian1101 <steventian1101@gmail.com>
2022-06-28 14:08:12 +0000

    Update create.blade.php

--- resources/views/backend/dashboard/products/create.blade.php
+++ resources/views/backend/dashboard/products/create.blade.php
@@ -74,24 +74,22 @@
 
                 <!-- Body -->
                 <div class="card-body">
-                    <div class="mb-4">
-                        <label class="row form-check form-switch mb-4" for="availabilitySwitch1">
-                            <span class="col-8 col-sm-9 ms-0">
-                                <span class="text-dark">Digital</span>
-                            </span>
-                            <span class="col-4 col-sm-3 text-end">
-                                <input type="checkbox" class="form-check-input" id="availabilitySwitch1">
-                            </span>
-                        </label>
-                        <label class="row form-check form-switch mb-4" for="availabilitySwitch1">
-                            <span class="col-8 col-sm-9 ms-0">
-                                <span class="text-dark">Virtual</span>
-                            </span>
-                            <span class="col-4 col-sm-3 text-end">
-                                <input type="checkbox" class="form-check-input" id="availabilitySwitch1">
-                            </span>
-                        </label>
-                    </div>
+                    <label class="row form-check form-switch mb-4" for="availabilitySwitch1">
+                        <span class="col-8 col-sm-9 ms-0">
+                            <span class="text-dark">Digital</span>
+                        </span>
+                        <span class="col-4 col-sm-3 text-end">
+                            <input type="checkbox" class="form-check-input" id="availabilitySwitch1">
+                        </span>
+                    </label>
+                    <label class="row form-check form-switch mb-4" for="availabilitySwitch1">
+                        <span class="col-8 col-sm-9 ms-0">
+                            <span class="text-dark">Virtual</span>
+                        </span>
+                        <span class="col-4 col-sm-3 text-end">
+                            <input type="checkbox" class="form-check-input" id="availabilitySwitch1">
+                        </span>
+                    </label>
                 </div>
             </div>
             <!-- End Card -->
