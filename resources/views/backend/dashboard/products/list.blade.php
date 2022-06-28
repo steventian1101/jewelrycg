ed02e6e657c8affba9b3134c581e840a10db8915
steventian1101 <steventian1101@gmail.com>
2022-06-28 15:55:12 +0000

    Update list.blade.php

--- resources/views/backend/dashboard/products/list.blade.php
+++ resources/views/backend/dashboard/products/list.blade.php
@@ -53,14 +53,23 @@
                                             <td>{{ $product->qty }}</td>
                                             <td>{{ $product->category }}</td>
                                             <td>
-                                                <a target="_blank" href="{{ route('products.show', $product->id) }}" class="btn btn-white btn-sm" 
-                                                    >
-                                                    <i class="bi-eye me-1"></i> Preview
-                                                </a>
-                                                <a href="{{ route('backend.products.edit', $product->id) }}" class="btn btn-white btn-sm" 
-                                                    >
-                                                    <i class="bi-pencil-fill me-1"></i> Edit
-                                                </a>
+                                                <div class="btn-group" role="group">
+                                                    <a class="btn btn-white btn-sm" href="{{ route('products.show', $product->id) }}"> <i class="bi-eye"></i> View </a>
+                                                    <!-- Button Group -->
+                                                    <div class="btn-group">
+                                                        <button type="button" class="btn btn-white btn-icon btn-sm dropdown-toggle dropdown-toggle-empty" id="ordersExportDropdown1" data-bs-toggle="dropdown" aria-expanded="false"></button>
+                                                        <div class="dropdown-menu dropdown-menu-end mt-1" aria-labelledby="ordersExportDropdown1" style="">
+                                                            <span class="dropdown-header">Options</span>
+                                                            <a href="{{ route('backend.products.edit', $product->id) }}" class="js-export-print dropdown-item">
+                                                                <i class="bi-pencil-fill me-1"></i> Edit Product
+                                                            </a>
+                                                            <div class="dropdown-divider"></div>
+                                                            <a class="dropdown-item" href="javascript:;"> <i class="bi-trash dropdown-item-icon"></i> Delete </a>
+                                                        </div>
+                                                    </div>
+                                                    <!-- End Unfold -->
+                                                </div>
+                                                <!-- End Button -->
                                             </td>
                                             </tr>
                                             @endforeach
