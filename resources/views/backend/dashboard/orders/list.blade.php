3ed431abb5ad5c4706dfc179b751588d0ee34832
steventian1101 <steventian1101@gmail.com>
2022-06-28 03:39:41 +0000

    Update list.blade.php

--- resources/views/backend/dashboard/orders/list.blade.php
+++ resources/views/backend/dashboard/orders/list.blade.php
@@ -1,4 +1,4 @@
-@extends('backend.dashboard.layouts.app', ['activePage' => 'products', 'title' => 'All Orders', 'navName' => 'orderlist', 'activeButton' => 'catalogue'])
+@extends('backend.dashboard.layouts.app', ['activePage' => 'products', 'title' => 'All Orders', 'navName' => 'orderslist', 'activeButton' => 'catalogue'])
 
 @section('content')
 <div class="card">
