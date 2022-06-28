fdf011dda94b5825c1a494d5c1eba63d20acc59a
steventian1101 <steventian1101@gmail.com>
2022-06-28 03:44:50 +0000

    Update list.blade.php

--- resources/views/backend/dashboard/users/list.blade.php
+++ resources/views/backend/dashboard/users/list.blade.php
@@ -1,4 +1,4 @@
-@extends('backend.dashboard.layouts.app', ['activePage' => 'users', 'title' => 'All Users', 'navName' => 'userslistj', 'activeButton' => 'laravel'])
+@extends('backend.dashboard.layouts.app', ['activePage' => 'users', 'title' => 'All Users', 'navName' => 'userslist', 'activeButton' => 'laravel'])
 
 @section('content')
     <!-- Page Header -->
