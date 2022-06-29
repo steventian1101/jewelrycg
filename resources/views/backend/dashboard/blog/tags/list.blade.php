2b665c05e2794f8f4ac9ee5ac4a6b3a4ff1441cb
steventian1101 <steventian1101@gmail.com>
2022-06-29 19:36:36 +0000

    Update list.blade.php

--- resources/views/backend/dashboard/blog/tags/list.blade.php
+++ resources/views/backend/dashboard/blog/tags/list.blade.php
@@ -4,7 +4,7 @@
     <!-- Page Header -->
     <div class="page-header">
         <div class="row align-items-end">
-            <h1 class="page-header-title">Blog Tags</h1>
+            <h1 class="page-header-title">Blog Tags <span class="badge bg-soft-dark text-dark ms-2">72,031</span></h1>
         </div>
         <!-- End Row -->
     </div>
