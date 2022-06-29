4d7a84f87314bd1e521ef4752133f03a12d9eccc
steventian1101 <steventian1101@gmail.com>
2022-06-29 20:12:27 +0000

    Update app.blade.php

--- resources/views/backend/dashboard/layouts/app.blade.php
+++ resources/views/backend/dashboard/layouts/app.blade.php
@@ -24,17 +24,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.1.0/ui/trumbowyg.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/css/bootstrap-select.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" />
-    <style data-hs-appearance-onload-styles>
-    *
-    {
-      transition: unset !important;
-    }
 
-    body
-    {
-      opacity: 0;
-    }
-    </style>
   </head>
   <body class="has-navbar-vertical-aside navbar-vertical-aside-show-xl   footer-offset">
 
