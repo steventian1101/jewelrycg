93771b23994e895b8e4a5ad1f779314a97c65df6
steventian1101 <steventian1101@gmail.com>
2022-06-29 17:19:48 +0000

    Update list.blade.php

--- resources/views/backend/dashboard/blog/categories/list.blade.php
+++ resources/views/backend/dashboard/blog/categories/list.blade.php
@@ -40,10 +40,21 @@
                                     <td>{{ $categorie->slug }}</td>
                                     <td>{{ $categorie->parent_id }}</td>
                                     <td>
-                                        <a href="{{ route('backend.blog.categories.edit', $categorie->id) }}" class="btn btn-white btn-sm" 
-                                            >
-                                            <i class="bi-pencil-fill me-1"></i> Edit
-                                    </a>
+                                        <div class="btn-group" role="group">
+                                            <a class="btn btn-white btn-sm" target="_blank" href="{{ route('backend.blog.categories.edit', $categorie->id) }}"> <i class="bi-eye"></i> Edit </a>
+                                            <!-- Button Group -->
+                                            <div class="btn-group">
+                                                <button type="button" class="btn btn-white btn-icon btn-sm dropdown-toggle dropdown-toggle-empty" id="ordersExportDropdown1" data-bs-toggle="dropdown" aria-expanded="false"></button>
+                                                <div class="dropdown-menu dropdown-menu-end mt-1" aria-labelledby="ordersExportDropdown1" style="">
+                                                    <span class="dropdown-header">Options</span>
+                                                    <div class="dropdown-divider"></div>
+                                                    <a class="dropdown-item" href="javascript:;"> <i class="bi-trash dropdown-item-icon"></i> Delete </a>
+                                                </div>
+                                            </div>
+                                            <!-- End Unfold -->
+                                        </div>
+                                        <!-- End Button -->
+
                                     </td>
                                     </tr>
                                     @endforeach
