8ce1f905b70427b300e939c182c9ed7c1b1c082b
steventian1101 <steventian1101@gmail.com>
2022-06-29 20:02:12 +0000

    Update sidebar.blade.php

--- resources/views/backend/dashboard/layouts/navbars/sidebar.blade.php
+++ resources/views/backend/dashboard/layouts/navbars/sidebar.blade.php
@@ -7,9 +7,6 @@
         <a class="navbar-brand" href="/backend" aria-label="Front">
           <img class="navbar-brand-logo" src="{{ asset('assets/svg/logos/logo.svg') }}" alt="Logo" data-hs-theme-appearance="default">
           <img class="navbar-brand-logo" src="{{ asset('assets/svg/logos-light/logo.svg') }}" alt="Logo" data-hs-theme-appearance="dark">
-          <img class="navbar-brand-logo-mini" src="{{ asset('assets/svg/logos/logo-short.svg') }}" alt="Logo" data-hs-theme-appearance="default">
-          <img class="navbar-brand-logo-mini" src="{{ asset('assets/svg/logos-light/logo-short.svg') }}" alt="Logo" data-hs-theme-appearance="dark">
-
         </a>
 
         <!-- End Logo -->
