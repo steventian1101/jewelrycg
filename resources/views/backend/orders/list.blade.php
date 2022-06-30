061cb1fdb5e8eaffb80289aa0baab07d106e843f
steventian1101 <steventian1101@gmail.com>
2022-06-30 14:44:51 +0000

    Update list.blade.php

--- resources/views/backend/orders/list.blade.php
+++ resources/views/backend/orders/list.blade.php
@@ -1,4 +1,4 @@
-@extends('backend.dashboard.layouts.app', ['activePage' => 'orders', 'title' => 'All Orders', 'navName' => 'orderslist', 'activeButton' => 'catalogue'])
+@extends('backend.layouts.app', ['activePage' => 'orders', 'title' => 'All Orders', 'navName' => 'orderslist', 'activeButton' => 'catalogue'])
 
 @section('content')
 <div class="page-header">
