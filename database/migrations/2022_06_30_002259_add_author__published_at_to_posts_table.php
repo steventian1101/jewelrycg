a729dfe68b4dfab505f031af8ab9acc0f2465a65
steventian1101 <steventian1101@gmail.com>
2022-06-30 02:24:11 +0200

    Add author & published_at to posts table

---
+++ database/migrations/2022_06_30_002259_add_author__published_at_to_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('blog_posts', function (Blueprint $table) {
+            $table->integer("author")->nullable();
+            $table->dateTime("published_at")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('blog_posts', function (Blueprint $table) {
+            $table->dropColumn('author');
+            $table->dropColumn('published_at');
+        });
+    }
+};
