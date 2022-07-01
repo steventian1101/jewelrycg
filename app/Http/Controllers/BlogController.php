6bdf5c0a24efa712c605741b656e52815ba88008
steventian1101 <steventian1101@gmail.com>
2022-07-01 17:31:22 +0000

    Update BlogController.php

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\BlogPost;
-//use App\BlogCategory;
-//use App\Blog;
+use App\Models\BlogTags;
+use App\Models\BlogCategorie;
+use App\Models\BlogPostTag;
+use App\Models\BlogPostCategorie;
 
 class BlogController extends Controller
 {
@@ -18,6 +20,15 @@ class BlogController extends Controller
             'posts' => BlogPost::with('categories')->get()
         ]);
     }
+    public function show($slug)
+    {
+        return view('blog.show', [
+            'post' => BlogPost::where('slug', $slug)->first(),
+            'categories' => BlogCategorie::all(),
+            'tags' => BlogTags::all()
+        ]);
+    }
+
 /*
     public function index() {
         $categories = BlogCategory::groupBy('slug')->get();
