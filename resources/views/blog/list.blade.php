31e084daf7823c5905c187e02eae7934abe2dfa9
steventian1101 <steventian1101@gmail.com>
2022-07-01 17:39:42 +0000

    Update list.blade.php

--- resources/views/blog/list.blade.php
+++ resources/views/blog/list.blade.php
@@ -1,56 +1,54 @@
 <x-app-layout page-title="Blog">
-<section class="p-6">
-    <div class="container text-center">
-        <div class="row">
-            <div class="col-lg-12 text-center">
-                <h1 class="fw-600 h4">Blog</h1>
-            </div>
-            <div class="col-lg-12">
-                <ul class="breadcrumb bg-transparent p-0 justify-content-center">
-                    <li class="breadcrumb-item opacity-50">
-                        <a class="text-reset" href="/">Home</a>
-                        
-                    </li>
-                    <li class="text-dark fw-600 breadcrumb-item">
-                        <a class="text-reset" href="/blog">"Blog"</a>
-                    </li>
-                </ul>
+    <section class="p-6">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h1 class="fw-600 h4">Blog</h1>
+                </div>
+                <div class="col-lg-12">
+                    <ul class="breadcrumb bg-transparent p-0 justify-content-center">
+                        <li class="breadcrumb-item opacity-50">
+                            <a class="text-reset" href="/">Home</a>
+                            
+                        </li>
+                        <li class="text-dark fw-600 breadcrumb-item">
+                            <a class="text-reset" href="/blog">"Blog"</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
-</section>
-<section class="bg-white pb-4">
-    <div class="container">
-        <div class="col-xl-11 mx-auto">
-            <div class="row gutters-10 row-cols-lg-3 row-cols-md-2 row-cols-1">
-                @foreach ($posts as $post)
+    </section>
+    <section class="bg-white pb-4">
+        <div class="container">
+            <div class="col-xl-11 mx-auto">
+                <div class="row gutters-10 row-cols-lg-3 row-cols-md-2 row-cols-1">
+                    @foreach ($posts as $post)
 
-                <div class="col mb-3">
-                    <div class="blog-post-list-container">
-                        <a href="/blog/{{ $post->slug }}" class="text-reset d-block">
-                            <img src="{{ asset('assets/img/placeholder-rect.jpg') }}" alt="{{ $post->name }}" class="img-blog-cropped border lazyloaded">
-                        </a>
-                        <div class="p-2 pt-3">
-                            <h2 class="fs-18 fw-600 mb-1">
-                                <a href="/blog/{{ $post->slug }}" class="text-reset article-list-title">
-                                    {{ $post->name }}
-                                </a>
-                            </h2>
-                            @foreach($post->categories as $category_info)
-                                <div class="mb-2 opacity-50 article-list-category">
-                                    <a href="/blog/{{ $post->slug }}" >{{$category_info->category->category_name}}</a>
-                                </div>
-                            @endforeach
-                            <p class="opacity-70 mb-4 article-list-excerpt">{{-- excerpt --}}</p>
+                    <div class="col mb-3">
+                        <div class="blog-post-list-container">
+                            <a href="/blog/{{ $post->slug }}" class="text-reset d-block">
+                                <img src="{{ asset('assets/img/placeholder-rect.jpg') }}" alt="{{ $post->name }}" class="img-blog-cropped border lazyloaded">
+                            </a>
+                            <div class="p-2 pt-3">
+                                <h2 class="fs-18 fw-600 mb-1">
+                                    <a href="/blog/{{ $post->slug }}" class="text-reset article-list-title">
+                                        {{ $post->name }}
+                                    </a>
+                                </h2>
+                                @foreach($post->categories as $category_info)
+                                    <div class="mb-2 opacity-50 article-list-category">
+                                        <a href="/blog/{{ $post->slug }}" >{{$category_info->category->category_name}}</a>
+                                    </div>
+                                @endforeach
+                                <p class="opacity-70 mb-4 article-list-excerpt">{{-- excerpt --}}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                @endforeach
+                    @endforeach
+                </div>
             </div>
         </div>
-    </div>
-</section>
-
-
+    </section>
 </x-app-layout>
