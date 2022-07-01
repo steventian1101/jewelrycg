d0ce28ae14b891c0dc12e7d4f9016e0247332b17
steventian1101 <steventian1101@gmail.com>
2022-07-01 16:47:17 +0000

    Update BlogController.php

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\BlogCategory;
-use App\Blog;
+use App\Models\BlogPost;
+//use App\BlogCategory;
+//use App\Blog;
 
 class BlogController extends Controller
 {
