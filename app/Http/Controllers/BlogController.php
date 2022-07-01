ca5c0f82dc9572193b326aa7a960affee401e2ef
steventian1101 <steventian1101@gmail.com>
2022-07-01 13:22:43 +0000

    Update BlogController.php

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -13,17 +13,17 @@ class BlogController extends Controller
     public function all_blog() {
         $categories = BlogCategory::groupBy('slug')->get();
         $blogs = Blog::where('status', 1)->orderBy('created_at', 'desc')->paginate(12);
-        return view("frontend.blog.listing", compact('blogs','categories'));
+        return view("blog.listing", compact('blogs','categories'));
     }
     public function category($slug)
     {
         $category = BlogCategory::where('slug', $slug)->first();
         $blogs = Blog::where('status', 1)->where('category_id',$category->id)->orderBy('created_at', 'desc')->paginate(12);
-        return view("frontend.blog.category", compact('blogs','category'));
+        return view("blog.category", compact('blogs','category'));
     }
     public function blog_details($slug) {
         $blog = Blog::where('slug', $slug)->first();
-        return view("frontend.blog.details", compact('blog'));
+        return view("blog.details", compact('blog'));
     }
 
 }
