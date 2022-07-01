1a39e66fa431509affab3c107041b59f812b4d6a
steventian1101 <steventian1101@gmail.com>
2022-07-01 17:16:53 +0000

    Update list.blade.php

--- resources/views/blog/list.blade.php
+++ resources/views/blog/list.blade.php
@@ -28,7 +28,7 @@
                 <div class="col mb-3">
                     <div class="blog-post-list-container">
                         <a href="/{{ $post->slug }}" class="text-reset d-block">
-                            <img src="{{ asset('assets/img/placeholder-rect.jpg') }}" alt="{{ $post->name }}" class="img-blog-cropped lazyloaded">
+                            <img src="{{ asset('assets/img/placeholder-rect.jpg') }}" alt="{{ $post->name }}" class="img-blog-cropped border lazyloaded">
                         </a>
                         <div class="p-2 pt-3">
                             <h2 class="fs-18 fw-600 mb-1">
@@ -51,81 +51,6 @@
         </div>
     </div>
 </section>
-
-
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            
-                        </div>
-                        <div class="table-responsive datatable-custom position-relative">
-
-                                <table class="table table-lg table-thead-bordered table-nowrap table-align-middle card-table dataTable no-footer">
-                                    <thead class="thead-light">
-                                        <th class="table-column-pe-0 sorting_disabled" aria-label="">
-                                            <div class="form-check">
-                                                <input class="form-check-input" type="checkbox" value="" id="datatableCheckAll">
-                                                <label class="form-check-label" for="datatableCheckAll"></label>
-                                            </div>
-                                        </th>
-                                        <th>ID</th>
-                                        <th>Name</th>
-                                        <th>Slug</th>
-                                        <th>Categories</th>
-                                        <th>Actions</th>
-                                    </thead>
-
-                                    <tbody>
-                                        @foreach ($posts as $post)
-                                        <tr>
-                                        <td class="table-column-pe-0">
-                                            <div class="form-check">
-                                                <input type="checkbox" class="form-check-input" id="ordersCheck1">
-                                                <label class="form-check-label" for="ordersCheck1"></label>
-                                            </div>
-                                        </td>
-                                        <td>{{ $post->id }}</td>
-                                       
-                                        <td>{{ $post->name }}</td>
-                                        <td>{{ $post->slug }}</td>
-                                        <td>
-                                            @foreach($post->categories as $category_info)
-                                                <p><span class="badge btn-info"> {{$category_info->category->category_name}} </span>  </p>
-                                            @endforeach
-                                        </td>
-                                        <td>
-                                            <div class="btn-group" role="group">
-                                                <a class="btn btn-white btn-sm" target="_blank" href="#"> <i class="bi-eye"></i> View </a>
-                                                <!-- Button Group -->
-                                                <div class="btn-group">
-                                                    <button type="button" class="btn btn-white btn-icon btn-sm dropdown-toggle dropdown-toggle-empty" id="ordersExportDropdown1" data-bs-toggle="dropdown" aria-expanded="false"></button>
-                                                    <div class="dropdown-menu dropdown-menu-end mt-1" aria-labelledby="ordersExportDropdown1" style="">
-                                                        <span class="dropdown-header">Options</span>
-                                                        <a href="{{ route('backend.posts.edit', $post->id) }}" class="js-export-print dropdown-item">
-                                                            <i class="bi-pencil-fill me-1"></i> Edit Post
-                                                        </a>
-                                                        <div class="dropdown-divider"></div>
-                                                        <a class="dropdown-item" href="javascript:;"> <i class="bi-trash dropdown-item-icon"></i> Delete </a>
-                                                    </div>
-                                                </div>
-                                                <!-- End Unfold -->
-                                            </div>
-                                            <!-- End Button -->
-
-                                        </td>
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                         
-                        </div>
-                    </div>
-                </div>
-
-
-            </div>
-
 
 
 </x-app-layout>
