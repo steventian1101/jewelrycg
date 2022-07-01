03319bcf1418350868437db91440146eebc5ccef
steventian1101 <steventian1101@gmail.com>
2022-07-01 16:53:19 +0000

    Update list.blade.php

--- resources/views/blog/list.blade.php
+++ resources/views/blog/list.blade.php
@@ -1,1 +1,94 @@
-glt
+<x-app-layout page-title="My Informations">
+
+<div class="page-header">
+    <div class="row align-items-center mb-3">
+        <div class="col-sm mb-2 mb-sm-0">
+            <h1 class="page-header-title">Post <span class="badge bg-soft-dark text-dark ms-2">72,031</span></h1>
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-auto">
+            <a class="btn btn-primary" href="{{ route('backend.posts.create') }}">Create post</a>
+        </div>
+        <!-- End Col -->
+    </div>
+    <!-- End Row -->
+</div>
+
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            
+                        </div>
+                        <div class="table-responsive datatable-custom position-relative">
+
+                                <table class="table table-lg table-thead-bordered table-nowrap table-align-middle card-table dataTable no-footer">
+                                    <thead class="thead-light">
+                                        <th class="table-column-pe-0 sorting_disabled" aria-label="">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="" id="datatableCheckAll">
+                                                <label class="form-check-label" for="datatableCheckAll"></label>
+                                            </div>
+                                        </th>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Slug</th>
+                                        <th>Categories</th>
+                                        <th>Actions</th>
+                                    </thead>
+
+                                    <tbody>
+                                        @foreach ($posts as $post)
+                                        <tr>
+                                        <td class="table-column-pe-0">
+                                            <div class="form-check">
+                                                <input type="checkbox" class="form-check-input" id="ordersCheck1">
+                                                <label class="form-check-label" for="ordersCheck1"></label>
+                                            </div>
+                                        </td>
+                                        <td>{{ $post->id }}</td>
+                                       
+                                        <td>{{ $post->name }}</td>
+                                        <td>{{ $post->slug }}</td>
+                                        <td>
+                                            @foreach($post->categories as $category_info)
+                                                <p><span class="badge btn-info"> {{$category_info->category->category_name}} </span>  </p>
+                                            @endforeach
+                                        </td>
+                                        <td>
+                                            <div class="btn-group" role="group">
+                                                <a class="btn btn-white btn-sm" target="_blank" href="#"> <i class="bi-eye"></i> View </a>
+                                                <!-- Button Group -->
+                                                <div class="btn-group">
+                                                    <button type="button" class="btn btn-white btn-icon btn-sm dropdown-toggle dropdown-toggle-empty" id="ordersExportDropdown1" data-bs-toggle="dropdown" aria-expanded="false"></button>
+                                                    <div class="dropdown-menu dropdown-menu-end mt-1" aria-labelledby="ordersExportDropdown1" style="">
+                                                        <span class="dropdown-header">Options</span>
+                                                        <a href="{{ route('backend.posts.edit', $post->id) }}" class="js-export-print dropdown-item">
+                                                            <i class="bi-pencil-fill me-1"></i> Edit Post
+                                                        </a>
+                                                        <div class="dropdown-divider"></div>
+                                                        <a class="dropdown-item" href="javascript:;"> <i class="bi-trash dropdown-item-icon"></i> Delete </a>
+                                                    </div>
+                                                </div>
+                                                <!-- End Unfold -->
+                                            </div>
+                                            <!-- End Button -->
+
+                                        </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                         
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+
+
+</x-app-layout>
