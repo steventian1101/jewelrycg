5018052bf157c0778ef14e603058c7a794c2a8e6
steventian1101 <steventian1101@gmail.com>
2022-07-01 17:38:50 +0000

    Update details.blade.php

---
+++ resources/views/blog/show.blade.php
@@ -0,0 +1,23 @@
+<section class="bg-white py-8">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-11 mx-auto">
+                <div class="blog-post-single-container">
+                    <div class="border-bottom mb-2">
+                        <h1 class="text-black fw-700 article-single-title">{{-- title --}}</h1>
+                        <div class="mb-2 opacity-50 article-single-category">
+                            <i>
+                                Published
+                                
+                            </i>
+                        </div>
+                    </div>
+                    <div class="mb-4 article-single-post overflow-hidden">
+                        {{-- content | widont --}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
