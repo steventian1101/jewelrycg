4ff7aaed6a933fc1a2c5a0c9544e268a431af69d
steventian1101 <steventian1101@gmail.com>
2022-07-01 18:24:00 +0000

    Update list.blade.php

--- resources/views/blog/category/list.blade.php
+++ resources/views/blog/category/list.blade.php
@@ -1,70 +1,31 @@
 <x-app-layout page-title="Blog">
-
-<div class="row">
-                <div class="col-md-12">
-                    <div class="card strpied-tabled-with-hover">
-                        <div class="card-header ">
-                            <div class="row">
-
-                            </div>
-                        </div>
-                        <div class="table-full-width table-responsive">
-                            <div class="col-md-12">
-                            <table class="table table-hover table-striped ">
-                                <thead>
-                                    <th class="table-column-pe-0 sorting_disabled" aria-label="">
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" value="" id="datatableCheckAll">
-                                            <label class="form-check-label" for="datatableCheckAll"></label>
-                                        </div>
-                                    </th>
-                                    <th class="sorting">ID</th>
-                                    <th>Name</th>
-                                    <th>Slug</th>
-                                    <th>Parent</th>
-
-                                    <th>Actions</th>
-                                </thead>
-                                <tbody>
-                                    @foreach ($categories as $categorie)
-                                    <tr>
-                                    <td class="table-column-pe-0">
-                                        <div class="form-check">
-                                            <input type="checkbox" class="form-check-input" id="ordersCheck1">
-                                            <label class="form-check-label" for="ordersCheck1"></label>
-                                        </div>
-                                    </td>
-                                    <td>{{ $categorie->id }}</td>
-                                    <td>{{ $categorie->category_name }}</td>
-                                    <td>{{ $categorie->slug }}</td>
-                                    <td>{{ $categorie->parent_id }}</td>
-                                    <td>
-                                        <div class="btn-group" role="group">
-                                            <a class="btn btn-white btn-sm" href="{{ route('backend.blog.categories.edit', $categorie->id) }}"> <i class="bi-pencil-fill p-1"></i> Edit </a>
-                                            <!-- Button Group -->
-                                            <div class="btn-group">
-                                                <button type="button" class="btn btn-white btn-icon btn-sm dropdown-toggle dropdown-toggle-empty" id="ordersExportDropdown1" data-bs-toggle="dropdown" aria-expanded="false"></button>
-                                                <div class="dropdown-menu dropdown-menu-end mt-1" aria-labelledby="ordersExportDropdown1" style="">
-                                                    <span class="dropdown-header">Options</span>
-                                                    <div class="dropdown-divider"></div>
-                                                    <a class="dropdown-item" href="javascript:;"> <i class="bi-trash dropdown-item-icon"></i> Delete </a>
-                                                </div>
-                                            </div>
-                                            <!-- End Unfold -->
-                                        </div>
-                                        <!-- End Button -->
-
-                                    </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                            </div>
-                        </div>
-                    </div>
+    <section class="p-6">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h1 class="fw-600 h4">Category List</h1>
                 </div>
-
-
+                <div class="col-lg-12">
+                    <ul class="breadcrumb bg-transparent p-0 justify-content-center">
+                        <li class="breadcrumb-item opacity-50">
+                            <a class="text-reset" href="/">Blog</a>
+                            
+                        </li>
+                        <li class="text-dark fw-600 breadcrumb-item">
+                            <a class="text-reset" href="/blog/category/list">"Category List"</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            
+        </div>
+    </section>
+    <section class="bg-white pb-4">
+        <div class="container">
+            <div class="col-xl-11 mx-auto">
+                @foreach ($categories as $categorie)
+                <a href="/blog/category/{{ $categorie->slug }}">{{ $categorie->category_name }}</a>
+                @endforeach
+            </div>
+        </div>
+    </section>     
 </x-app-layout>
