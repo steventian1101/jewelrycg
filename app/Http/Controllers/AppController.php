f840a0c1bce0c79ae8848da5677224b295f6928a
steventian1101 <steventian1101@gmail.com>
2022-07-02 23:39:33 +0000

    Update AppController.php

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -9,7 +9,6 @@ class AppController extends Controller
     public function index()
     {
         $products = cache()->remember('todays-deals', 60*60*24, fn() => Product::getTodaysDeals());
-        $product->setPriceToFloat();
         return view('index', compact('products'));
     }
 }
