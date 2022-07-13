40444bdac8bef1dca96cff273c73e329bd1bd8fc
steventian1101 <steventian1101@gmail.com>
2022-07-13 23:41:31 +0000

    Update sidebar.blade.php

--- resources/views/backend/layouts/navbars/sidebar.blade.php
+++ resources/views/backend/layouts/navbars/sidebar.blade.php
@@ -1,5 +1,5 @@
 <!-- Navbar Vertical -->
-  <aside class="sidebar col-auto">
+  <aside class="sidebar col-lg-auto col-sm-12">
     <div class="navbar-vertical-container">
       <div class="navbar-vertical-footer-offset">
 
