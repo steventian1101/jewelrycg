b6a8951b222614a588ab5eb9321f58cbfb9a0cdf
steventian1101 <steventian1101@gmail.com>
2022-07-13 16:30:47 +0000

    Update list.blade.php

--- resources/views/backend/blog/tags/list.blade.php
+++ resources/views/backend/blog/tags/list.blade.php
@@ -54,7 +54,7 @@
         </div>
 
         <div class="col-lg-8">
-            <div class="card mb-4">
+            <div class="card rounded-0 mb-4">
                 <div class="card-header card-header-content-md-between">
                     <div class="mb-2 mb-md-0">
                         <h3 class="card-header-title">Tags</h3>
@@ -62,7 +62,7 @@
                 </div>
                 <div class="table-responsive datatable-custom position-relative">
                     <div id="datatable_wrapper" class="dataTables_wrapper no-footer">
-                        <table id="datatable" class="table table-lg table-borderless table-thead-bordered table-nowrap table-align-middle card-table dataTable no-footer" role="grid" aria-describedby="datatable_info">
+                        <table id="datatable" class="table table-lg table-thead-bordered table-nowrap table-align-middle card-table dataTable table-hover no-footer" role="grid" aria-describedby="datatable_info">
                             <thead class="thead-light">
                                 <tr role="row">
                                     <th class="table-column-pe-0 sorting_disabled" aria-label="">
