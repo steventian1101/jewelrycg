9107d5e44571bfdbeb67c01aafc0be1540cc75e4
steventian1101 <steventian1101@gmail.com>
2022-07-13 00:13:47 +0000

    Update navbar.blade.php

--- resources/views/backend/layouts/navbars/navbar.blade.php
+++ resources/views/backend/layouts/navbars/navbar.blade.php
@@ -1,15 +1,33 @@
 <!-- ========== HEADER ========== -->
-<header id="header" class="navbar navbar-fixed navbar-bordered bg-white">
-  <div class="container">
-    <!-- Logo -->
-    <a class="navbar-brand" href="/backend" aria-label="Front">
-    <!--<img class="navbar-brand-logo" src="{{ asset('assets/svg/logos/logo.svg') }}" alt="Logo" data-hs-theme-appearance="default">-->
-    #JewelryCG
-    </a>
-    <!-- End Logo -->
-
-
-  </div>
+<header class="navbar navbar-expand-lg py-3">
+    <nav class="container">
+        <a class="navbar-brand col-auto" href="https://jewelrycadfiles.com">
+            #JewelryCG
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown menu-area">
+                    <a class="nav-link active" href="https://jewelrycadfiles.com">Home</a>
+                </li>
+                <li class="nav-item dropdown menu-area">
+                    <a class="nav-link active" href="https://jewelrycadfiles.com/3d-models">3D Models</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link active dropdown-toggle" aria-current="page" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" href="#">Learn</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="https://jewelrycadfiles.com/blog">Blog</a></li>
+                        <li><a class="dropdown-item" href="https://jewelrycadfiles.com/blog/category/list/all">Categories</a></li>
+                        <li><a class="dropdown-item" href="https://jewelrycadfiles.com/blog/tag/list/all">Tags</a></li>
+                        <li><a class="dropdown-item" href="#">Comments</a></li>
+                    </ul>
+                </li>
+                
+            </ul>
+        </div>
+    </nav>
 </header>
 
   <!-- ========== END HEADER ========== -->
