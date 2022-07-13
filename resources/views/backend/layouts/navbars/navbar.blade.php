c84915e3ed82c16f4bd23200e1725f56e8a38be6
steventian1101 <steventian1101@gmail.com>
2022-07-13 00:10:01 +0000

    Update navbar.blade.php

--- resources/views/backend/layouts/navbars/navbar.blade.php
+++ resources/views/backend/layouts/navbars/navbar.blade.php
@@ -1,6 +1,6 @@
 <!-- ========== HEADER ========== -->
   <header id="header" class="navbar navbar-fixed navbar-bordered bg-white">
-    <div class="navbar-nav-wrap">
+    <div class="container">
       <!-- Logo -->
       <a class="navbar-brand" href="/backend" aria-label="Front">
         <!--<img class="navbar-brand-logo" src="{{ asset('assets/svg/logos/logo.svg') }}" alt="Logo" data-hs-theme-appearance="default">-->
