8e9ad555dca47d468df4511a5796ca018106bd1a
steventian1101 <steventian1101@gmail.com>
2022-07-14 01:05:01 +0000

    Update list.blade.php

--- resources/views/backend/products/list.blade.php
+++ resources/views/backend/products/list.blade.php
@@ -54,7 +54,7 @@
                                             <td>{{ $product->product_category->category_name }}</td>
                                             <td>
                                                 <div class="btn-group" role="group">
-                                                    <a class="btn btn-dark btn-sm" target="_blank" href="{{ route('backend.products.edit', $product->id) }}"> <i class="bi-pencil"></i> Edit </a>
+                                                    <a class="btn btn-dark btn-sm" href="{{ route('backend.products.edit', $product->id) }}"> <i class="bi-pencil"></i> Edit </a>
                                                     <!-- Button Group -->
                                                     <div class="btn-group">
                                                         <button type="button" class="btn btn-dark btn-icon btn-sm dropdown-toggle dropdown-toggle-empty" id="ordersExportDropdown1" data-bs-toggle="dropdown" aria-expanded="false"></button>
