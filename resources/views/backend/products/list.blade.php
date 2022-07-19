51eeb6b653ac05e54e9e3706a639ad7c8712718e
steventian1101 <steventian1101@gmail.com>
2022-07-19 00:04:50 +0000

    Update list.blade.php

--- resources/views/backend/products/list.blade.php
+++ resources/views/backend/products/list.blade.php
@@ -20,9 +20,9 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="card strpied-tabled-with-hover rounded-0">
-                        <div class="table-responsive datatable-custom">
+                        <div class="datatable-custom">
                             <div class="col-md-12">
-                                <table class="table table-lg table-thead-bordered table-nowrap table-align-middle card-table dataTable table-hover no-footer">
+                                <table class="table table-lg table-thead-bordered table-nowrap table-align-middle card-table dataTable table-hover table-responsive no-footer">
                                     <thead class="thead-light">
                                         <th class="sorting_disabled" aria-label="">
                                             <div class="form-checkbox">
