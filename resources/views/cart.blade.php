b53b0ea12cfacc52a89d6f7c84f1fd7c1ddf69e0
steventian1101 <steventian1101@gmail.com>
2022-07-20 04:41:08 +0000

    Update cart.blade.php

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,12 +1,16 @@
 <x-app-layout page-title="Cart">
-    <div class="card">
-        @if (session('message'))
-            <h4 class="text-center text-danger mt-3">
-                {{session('message')}}
-            </h4>
-        @endif
-        <div class="card-body">
-            <x-products-table/>
-        </div>
-    </div>        
+<div class="py-6 cart-wrap">
+    <div class="container">
+        <div class="card">
+            @if (session('message'))
+                <h4 class="text-center text-danger mt-3">
+                    {{session('message')}}
+                </h4>
+            @endif
+            <div class="card-body">
+                <x-products-table/>
+            </div>
+        </div>  
+    </div>
+</div>      
 </x-app-layout>
