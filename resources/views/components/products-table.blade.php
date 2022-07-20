08f9d38d6718d599c60f5256d42d2424362ff68e
steventian1101 <steventian1101@gmail.com>
2022-07-20 04:43:33 +0000

    Update products-table.blade.php

--- resources/views/components/products-table.blade.php
+++ resources/views/components/products-table.blade.php
@@ -37,7 +37,7 @@
                                     @csrf
                                     @method('put')
                                     <input type="hidden" name="row_id" value="{{$product->rowId}}">
-                                    <button type="submit" class="btn btn-outline-primary" title="Add to Cart">
+                                    <button type="submit" class="btn btn-primary" title="Add to Cart">
                                         <i class="fa-solid fa-cart-arrow-down"></i>
                                     </button>
                                 </form>
@@ -81,7 +81,7 @@
                                     @method('patch')    
                                     <input type="hidden" name="row_id" value="{{$product->rowId}}">
                                     <span class="col-7" align="end">
-                                        <button type="submit" class="btn btn-outline-primary" title="Edit quantity"><i class="fas fa-edit"></i></button>
+                                        <button type="submit" class="btn btn-primary" title="Edit quantity"><i class="bi bi-pencil-square"></i></button>
                                         <button formaction="{{route('cart.remove.product')}}" type="submit" class="btn btn-outline-danger" title="Remove from chart"><i class="fa-solid fa-x"></i></button>
                                     </span>        
                                 </div>
