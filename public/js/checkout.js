9aa7cfaf870f4d517c32a3289d9da7793f9930d3
steventian1101 <steventian1101@gmail.com>
2022-07-24 12:05:40 -0700

    hello

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -100,7 +100,7 @@
           // your `return_url`. For some payment methods like iDEAL, your customer will
           // be redirected to an intermediate site first to authorize the payment, then
           // redirected to the `return_url`.
-          if (error.type === "card_error" || error.type === "validation_error") {
+          if (error.type === "card_error" || error.type === "validation_error" || error.type === 'invalid_request_error') {
             showMessage(error.message);
           } else {
             showMessage("An unexpected error occurred.");
