0e66c8d008b874aa828cc74c7f021ad0ba095884
steventian1101 <steventian1101@gmail.com>
2022-07-26 23:58:10 +0000

    Update list.blade.php

--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -12,33 +12,6 @@
 
         <x-products-display :products="$products"/>
         
-        <div class="row row-cols-xxl-6 row-cols-xl-6 row-cols-lg-4 row-cols-md-4 row-cols-2">
-            @foreach ($products as $product)
-            <div class="col">
-                <a class="card hov-shadow-sm mt-1 mb-2 has-transition bg-white p-2" href="{{route('products.show', $product->slug) }}">
-                    <div class="w-100 mb-2 pb-3 border-bottom">
-                        @if($product->uploads->file_name == 'none.png')
-                            <img src="{{ asset('assets/img/placeholder.jpg') }}" alt="{{ $product->name }}" class="rounded-sm w-100 lazyloaded">
-                        @else
-                            <img src="{{ asset('uploads/all') }}/{{$product->uploads->file_name}}" alt="{{ $product->name }}" class="rounded-sm w-100 lazyloaded">
-                        @endif
-                    </div>
-                    <div class="text-left px-2">
-                        <div class="fs-15">
-                            <span class="fw-700 text-primary">
-                                {{ $product->price }}
-                            </span>
-                        </div>
-                        <h3 class="fw-600 fs-13 text-truncate-2 lh-1-4 mb-0 h-35px text-black">
-                            {{ $product->name }}
-                        </h3>
-                    </div>
-                </a>
-            </div>
-            @endforeach
-        </div>
-
-     
         <div class="mt-16 mb-6 border-t mx-auto py-8">
             <div class="flex items-center justify-between">
                 <div>
