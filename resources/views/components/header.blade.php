5870d0ea9eb0a9d05c9da2754efc8e6bdd1995b1
steventian1101 <steventian1101@gmail.com>
2022-07-26 00:21:56 +0000

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -11,7 +11,8 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-            <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+            <!-- left navbar-->
+            <ul class="navbar-nav mb-2 mb-lg-0">
                 <li class="nav-item dropdown menu-area">
                     <a class="nav-link active" href="{{ route('index') }}">Home</a>
                 </li>
@@ -27,25 +28,6 @@
                         <li><a class="dropdown-item" href="#">Comments</a></li>
                     </ul>
                 </li>
-                <li class="nav-item dropdown menu-area">
-                    <a href="{{route('cart.index')}}" class="nav-link">
-                        <i class="bi bi-cart2"></i>
-                        <?php
-                            if(Cart::instance('default')->content()->count() == 0
-                                && auth()->check()
-                            )
-                            {
-                                Cart::merge(auth()->id());
-                            }
-                        ?>
-                        @if ($cart_items = Cart::content()->count())
-                            <span class="rounded-pill pill badge bg-primary text-light">
-                                {{$cart_items}}
-                            </span>
-                        @endif
-                    </a>
-                </li>
-
                 @auth
                 <li class="nav-item dropdown">
                     <a class="nav-link active dropdown-toggle" aria-current="page" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" href="#">{{ Auth::user()->first_name }}</a>
@@ -67,6 +49,42 @@
                 </li>
                 @endauth
             </ul>
+            <!-- end left navbar-->
+
+            <!-- right navbar-->
+            <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown menu-area">
+                    <a href="{{route('cart.index')}}" class="nav-link">
+                        <i class="bi bi-cart2"></i>
+                        <?php
+                            if(Cart::instance('default')->content()->count() == 0
+                                && auth()->check()
+                            )
+                            {
+                                Cart::merge(auth()->id());
+                            }
+                        ?>
+                        @if ($cart_items = Cart::content()->count())
+                            <span class="rounded-pill pill badge bg-primary text-light">
+                                {{$cart_items}}
+                            </span>
+                        @endif
+                    </a>
+                </li>
+
+                @auth
+
+                @else
+                <li class="nav-item ml-1">
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>  
+                </li>
+                <li class="nav-item ml-1">
+                    <a class="nav-link" href="{{ route('register') }}">Register</a>  
+                </li>
+                @endauth
+            </ul>
+            <!--end right nav-->
+
         </div>
     </nav>
 </header>
