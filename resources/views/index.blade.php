d1088c48019f6f6fac2f055d86e3c4a07fdecc13
steventian1101 <steventian1101@gmail.com>
2022-07-28 06:37:37 +0000

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -45,11 +45,13 @@
             </div>
         </div>
     </div>
+    <!--
     <div class="hero-media" id="hero-media">
       <video class="hero-media-asset is-visible" autoplay="" muted="" loop="" playsinline="" data-hero-video="" data-src-lg="https://cdn.dribbble.com/uploads/39417/original/49dbf46eae15d227fc95a69cee31251e.mp4?1657824906" data-src-sm="https://cdn.dribbble.com/uploads/39418/original/0cc960a3bf612d0badc4f6165eb36f7b.mp4?1657824915">
         <source src="https://cdn.dribbble.com/uploads/39417/original/49dbf46eae15d227fc95a69cee31251e.mp4?1657824906" type="video/mp4">
       </video>
   </div>
+  -->
 </section>
 <!--
 <section class="why-points top-content-container pt-4 pb-4">
