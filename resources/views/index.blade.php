caba541bbd6ac074d55c5189f27bf0233f470de2
steventian1101 <steventian1101@gmail.com>
2022-07-28 07:02:29 +0000

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,9 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6 mx-auto text-center">
-                <h4 class="fs-20 text-white mb-4">The world's preferred source for Jewelry CG content</h4>
-                <h1 class="text-white font-weight-bold mb-4">Explore our vast collections of 3D models</h1>
-                <div class="search-form ml-auto mr-auto">
+                <h4 class="fs-20 text-white py-4 mb-0">The world's preferred source for Jewelry CG content</h4>
+                <h1 class="text-white font-weight-bold pb-4 mb-0">Explore our vast collections of 3D models</h1>
+                <div class="search-form ml-auto mr-auto py-2">
                     <form method="get" action="{{route('products.search')}}">
                         <div class="search-col">
                             <input name="q" type="search" placeholder="Search" aria-label="Search" class="search-control">
