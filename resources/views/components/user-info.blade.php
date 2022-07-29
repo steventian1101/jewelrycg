4eccd10fb854ad24e03dd841948bde66a67d0957
steventian1101 <steventian1101@gmail.com>
2022-07-29 23:13:04 +0000

    Update user-info.blade.php

--- resources/views/components/user-info.blade.php
+++ resources/views/components/user-info.blade.php
@@ -7,46 +7,30 @@
     <input type="text" name="address2" value="{{ auth()->user()->address->address2 ?? old('address2') }}" id="floatingAddress2" class="form-control" placeholder="Enter Secondary Address (optional)">
 </div>
 <div class="form-floating mb-3">
-    <label for="city" class="offset-md-1 col-sm-2 col-form-label">City</label>
-    <div class="col-sm-8">
-        <input type="text" name="city" value="{{ auth()->user()->address->city ?? old('city') }}" id="city"
-            class="form-control" required placeholder="Enter City">
-    </div>
+    <label for="floatingCity">City</label>
+    <input type="text" name="city" value="{{ auth()->user()->address->city ?? old('city') }}" id="floatingCity city" class="form-control" required placeholder="Enter City">
 </div>
 <div class="form-floating mb-3">
-    <label for="state" class="offset-md-1 col-sm-2 col-form-label">State</label>
-    <div class="col-sm-8">
-        <input type="text" name="state" value="{{ auth()->user()->address->state ?? old('state') }}" id="state"
-            class="form-control" required placeholder="Enter State">
-    </div>
+    <label for="floatingState">State</label>
+    <input type="text" name="state" value="{{ auth()->user()->address->state ?? old('state') }}" id="floatingState state" class="form-control" required placeholder="Enter State">
 </div>
 <div class="form-floating mb-3">
-    <label class="offset-md-1 col-sm-2 col-form-label" for="country">Country</label>
-    <div class="col-sm-8">
-        <select name="country" id="" data-live-search="true" class="form-control">
-            @foreach ($countries as $country)
-                @if (auth()->user()->address && auth()->user()->address->country == $country->code)
-                    <option value="{{ $country->code }}" selected>{{ $country->name }}</option>
-                @else
-                    <option value="{{ $country->code }}">{{ $country->name }}</option>
-                @endif
-            @endforeach
-        </select>
-        {{-- <input type="text" name="country" value="{{ auth()->user()->address->country ?? old('country') }}" id="country"
-            class="form-control" required placeholder="Enter Country"> --}}
-    </div>
+    <label for="floatingCountry">Country</label>
+    <select name="country" id="floatingCountry" data-live-search="true" class="form-control">
+        @foreach ($countries as $country)
+            @if (auth()->user()->address && auth()->user()->address->country == $country->code)
+                <option value="{{ $country->code }}" selected>{{ $country->name }}</option>
+            @else
+                <option value="{{ $country->code }}">{{ $country->name }}</option>
+            @endif
+        @endforeach
+    </select>
 </div>
 <div class="form-floating mb-3">
-    <label class="offset-md-1 col-sm-2 col-form-label" for="pin_code">PIN Code</label>
-    <div class="col-sm-8">
-        <input type="text" name="pin_code" value="{{ auth()->user()->address->postal_code ?? old('pin_code') }}"
-            id="pin_code" class="form-control" required placeholder="Enter PIN Code">
-    </div>
+    <label for="floatingZipcode">Zipcode</label>
+    <input type="text" name="pin_code" value="{{ auth()->user()->address->postal_code ?? old('pin_code') }}" id="floatingZipcode pin_code" class="form-control" required placeholder="Enter PIN Code">
 </div>
 <div class="form-floating mb-3">
-    <label class="offset-md-1 col-sm-2 col-form-label" for="phone">Phone Number</label>
-    <div class="col-sm-8">
-        <input type="tel" name="phone" value="{{ auth()->user()->address->phone ?? old('phone') }}" id="phone"
-            class="form-control" placeholder="Enter Phone Number">
-    </div>
+    <label for="floatingPhonenumber">Phone Number</label>
+    <input type="tel" name="phone" value="{{ auth()->user()->address->phone ?? old('phone') }}" id="floatingPhonenumber phone" class="form-control" placeholder="Enter Phone Number">
 </div>
