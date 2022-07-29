b11809dc259aea332cba347ddedd29f4edb69ff6
steventian1101 <steventian1101@gmail.com>
2022-07-29 23:36:00 +0000

    Update billing.blade.php

--- resources/views/checkout/billing.blade.php
+++ resources/views/checkout/billing.blade.php
@@ -6,10 +6,10 @@
                     <div class="logo py-4 fw-800 fs-24">#JEWELRYCG</div>
                     <nav class="pb-4" aria-label="breadcrumb">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="#">Cart</a></li>
-                            <li class="breadcrumb-item"><a href="#">Shipping</a></li>
+                            <li class="breadcrumb-item"><a href="/cart">Cart</a></li>
+                            <li class="breadcrumb-item"><a href="/checkout/shipping">Shipping</a></li>
                             <li class="breadcrumb-item active" aria-current="page">Billing</li>
-                            <li class="breadcrumb-item active" aria-current="page">Payment</li>
+                            <li class="breadcrumb-item" aria-current="page">Payment</li>
                         </ol>
                     </nav>
 
