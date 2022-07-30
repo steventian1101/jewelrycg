88af674ffe515af8412b6c93b48f49354bc83cde
steventian1101 <steventian1101@gmail.com>
2022-07-30 05:17:01 +0000

    Update orders-table.blade.php

--- resources/views/components/orders-table.blade.php
+++ resources/views/components/orders-table.blade.php
@@ -12,11 +12,11 @@
             </div>
             <div class="col-lg-2">
                 <div class="fw-600">Items</div>
-                <span>$115.00</span>
+                <span>{{$order->items_count}}</span>
             </div>
             <div class="col-lg-2">
                 <div class="fw-600">Total amount</div>
-                <span>$115.00</span>
+                <span>${{$order->total_price}}</span>
             </div>
             <div class="col-lg-2 ml-auto">
                 <a href="{{route('orders.show', $order->order_id)}}" class="btn btn-primary">View Order</a>
@@ -28,42 +28,3 @@
     </div>
 </div>
 @endforeach
-
-<table class="table">
-    <thead>
-        <tr>
-            @if (auth()->user()->is_admin)
-                <th scope="col">User Id</th>
-            @endif
-            <th scope="col">Status</th>
-            <th scope="col">Message</th>
-            <th scope="col">Products</th>
-            <th scope="col">Total</th>
-            <th scope="col"></th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($orders as $order)
-            <tr>
-                @if (auth()->user()->is_admin)
-                    <td> 
-                        <a href="{{route('user.index', $order->user_id)}}" class="link-primary">
-                            {{$order->user->email}}
-                        </a>
-                    </td>
-                @endif
-                <td class="link-info">{{$order->status_payment == config('constants.payment_status.paid') ? 'paid' : ($order->status_payment == config('constants.payment_status.unpaid') ? 'unpaid' : 'rejected')}}</td>
-                @if ($order->status_payment == config('constants.payment_status.reject'))
-                    <td>{{$order->status_payment_reason}}</td>
-                @endif
-                <td>{{$order->items_count}}</td>
-                <td>${{$order->total_price}}</td>
-                <td>
-                    <div class="d-flex justify-content-center">
-                        <a href="{{route('orders.show', $order->order_id)}}" class="btn btn-primary">Details</a>
-                    </div>
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
