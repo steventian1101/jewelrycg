ba5342ec980f90d0573e6678a7e5bfce8c882689
steventian1101 <steventian1101@gmail.com>
2022-07-30 02:18:19 +0000

    Update show.blade.php

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -1,36 +1,37 @@
 <x-app-layout :page-title="'Order ' . $order->id">
     <div class="container">
         <div class="col-lg-8 col-md-10 py-8 mx-auto checkout-wrap">
-            <h1>Thanks for shopping with us!</h1>
+            <h1 class="fw-800">Thanks for shopping with us!</h1>
             <p>We appreciate your order, we’re currently processing it. So hang tight, and we’ll send you confirmation very soon!</p>
-
-            <div class="row">
-                <div class="col-lg-3 col-6">
-                    <div class="w-100 fs-18 fw-600">Order number</div>
-                    <div class="fs-14 text-primary">#0000</div>
-                </div>
-                <div class="col-lg-3 col-6">
-                    <div class="w-100 fs-18 fw-600">Payment status</div>
-                    <div class="fs-14 ">Paid</div>
-                </div>
-                <div class="col-lg-3 col-6">
-                    <div class="w-100 fs-18 fw-600">Fufilment status</div>
-                    <div class="fs-14 ">#0000</div>
-                </div>
-                <div class="col-lg-3 col-6">
-                    <div class="w-100 fs-18 fw-600">Date created</div>
-                    <div class="fs-14 ">July 30, 2022</div>
+            <div class="order-items-card py-4">
+                <div class="row">
+                    <div class="col-lg-3 col-6">
+                        <div class="w-100 fs-18 fw-600">Order number</div>
+                        <div class="fs-14 text-primary">#0000</div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="w-100 fs-18 fw-600">Payment status</div>
+                        <div class="fs-14 ">Paid</div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="w-100 fs-18 fw-600">Fufilment status</div>
+                        <div class="fs-14 ">#0000</div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="w-100 fs-18 fw-600">Date created</div>
+                        <div class="fs-14 ">July 30, 2022</div>
+                    </div>
                 </div>
             </div>
 
             @foreach ($order->items as $key => $item)
-                <div class="order-items-card">
+                <div class="order-items-card pb-4">
                     <div class="row">
-                        <div class="col-lg-3">
-                            <img src="{{ asset('uploads/all/' . $item->product->uploads->file_name) }}" alt="" class="thumbnail w-100">
+                        <div class="col-lg-2">
+                            <img src="{{ asset('uploads/all/' . $item->product->uploads->file_name) }}" alt="" class="thumbnail border w-100">
                         </div>
                         <div class="col-lg-7">
-                            <div class="order-item-title">
+                            <div class="order-item-title fs-24 mb-2 fw-600">
                             @php
                                 if ($item->product_variant != 0) {
                                     echo $item->product_name . ' - ' . $item->product_variant_name;
