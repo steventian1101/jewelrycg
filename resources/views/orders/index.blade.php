10e551d1ccad67f17effae0dc471a3ab405c48df
steventian1101 <steventian1101@gmail.com>
2022-07-30 04:42:53 +0000

    Update index.blade.php

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,12 +1,19 @@
 <x-app-layout page-title="My Orders">
-    <div class="p-3">
-        <div class="card ">
-            <div class="card-body">
-                <x-orders-table :orders="$orders"/>
-            </div>
-        </div>        
-        <div class="text-center">
-            {{$orders->links()}}
-        </div>    
+<div class="container">
+    <div class="col-lg-8 col-md-10 py-8 mx-auto checkout-wrap">
+        <h1 class="fw-800">Thanks for shopping with us!</h1>
+        <p>We appreciate your order, we’re currently processing it. So hang tight, and we’ll send you confirmation very soon!</p>
+
+        <div class="p-3">
+            <div class="card ">
+                <div class="card-body">
+                    <x-orders-table :orders="$orders"/>
+                </div>
+            </div>        
+            <div class="text-center">
+                {{$orders->links()}}
+            </div>    
+        </div>
     </div>
+</div>
 </x-app-layout>
