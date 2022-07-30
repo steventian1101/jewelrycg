0e40985e4182175dd71929ce176b396d5285c36a
steventian1101 <steventian1101@gmail.com>
2022-07-30 11:50:06 -0700

    completed the order detailed page

--- config/constants.php
+++ config/constants.php
@@ -6,9 +6,16 @@ return [
     ],
     'product_thumbnail_suffix' => '_thumbnail',
     'order_item_status_fulfillment' => [
+        '',
         'pending',
         'shipped',
         'delivered'
+    ],
+    'oder_payment_status' => [
+        '',
+        'unpaid',
+        'paid',
+        'reject'
     ],
     'payment_status' => [
         'unpaid' => 1,
