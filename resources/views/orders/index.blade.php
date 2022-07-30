3a310542db0071116c827fe16b518517cb2cfa72
steventian1101 <steventian1101@gmail.com>
2022-07-30 05:17:54 +0000

    Update index.blade.php

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -2,7 +2,7 @@
 <div class="container">
     <div class="col-lg-8 col-md-10 py-8 mx-auto checkout-wrap">
         <h1 class="fw-800">Order history</h1>
-        <p>Check the status of recent orders, view order details, and fufilment status.</p>
+        <p class="pb-4">Check the status of recent orders, view order details, and fufilment status.</p>
 
         <x-orders-table :orders="$orders"/>
         {{$orders->links()}}
