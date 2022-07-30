fbcff7c211b7f926589277b88372a7ab6b6bfad2
steventian1101 <steventian1101@gmail.com>
2022-07-30 03:47:03 +0000

    Update show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -136,34 +136,9 @@
                             </div>
 
                             <div class="product-details-misc mb-4">
-                                @if (session('wishlist-message'))
-                                    <h4 class="text-center text-success">
-                                        {{session('wishlist-message')}}
-                                    </h4>
-                                @endif
+                                
                                 <h4>
-                                    @auth
-                                        @if ($wishlist_product = Cart::instance('wishlist')->content()->firstWhere('id', $product->id))
-                                            <form action="{{route('cart.wishlist')}}" method="post" class="d-inline">
-                                                @method('delete')
-                                                @csrf
-                                                <input type="hidden" name="row_id" value="{{$wishlist_product->rowId}}">
-                                                <button type="submit" class="badge badge-lg bg-danger-1 border border-danger-1 text-light rounded-pill" style="border: 1px solid grey !important;color: black !important;">
-                                                    <i class="fa-solid fa-x"></i>
-                                                    <small>Remove from Wishlist</small>
-                                                </button>
-                                            </form>
-                                        @else
-                                            <form action="{{route('cart.wishlist')}}" method="post" class="d-inline">
-                                                @csrf
-                                                <input type="hidden" name="id_product" value="{{$product->id}}">
-                                                <button type="submit" class="badge badge-lg bg-primary border border-primary text-light rounded-pill">
-                                                    <i class="fa-regular fa-heart"></i>
-                                                    <small>Add To Wishlist</small>
-                                                </button>
-                                            </form>
-                                        @endif
-                                    @endauth
+
                                     @if ($product->is_trackingquantity)
                                         @if ($product->quantity)
                                             <span class="badge badge-lg bg-success text-light rounded-pill"><small>On Stock: {{$product->quantity}}</small></span>
@@ -275,6 +250,15 @@
             </div>
         </div>
     </section>
+
+    <!--
+    @if (session('wishlist-message'))
+        <h4 class="text-center text-success">
+            {{session('wishlist-message')}}
+        </h4>
+    @endif
+    -->
+    
     <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>    <script>
