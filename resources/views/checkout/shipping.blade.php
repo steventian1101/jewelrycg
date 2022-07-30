675903d02db44f49fdc34dcdc6e3b1c027feeef0
steventian1101 <steventian1101@gmail.com>
2022-07-30 00:39:12 +0000

    Update shipping.blade.php

--- resources/views/checkout/shipping.blade.php
+++ resources/views/checkout/shipping.blade.php
@@ -25,7 +25,7 @@
                                                 id="option{{ $i }}" autocomplete="off"
                                                 @if ($i == 0) checked @endif
                                                 data-price="{{ $shipping->price / 100 }}" />
-                                            <label class="card shipping-method-btn p-3 shipping-radio" for="option{{ $i }}"
+                                            <label class="card btn shipping-method-btn p-3 shipping-radio" for="option{{ $i }}"
                                                 style="width: 100%;">
                                                 <span>{{ $shipping->name }} ({{ $shipping->description }})</span>
                                                 <span class="float-right">${{ $shipping->price / 100 }}</span>
