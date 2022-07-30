e31446f305a3f1f322c44810de1219b16159aa57
steventian1101 <steventian1101@gmail.com>
2022-07-30 05:21:12 +0000

    Update orders-table.blade.php

--- resources/views/components/orders-table.blade.php
+++ resources/views/components/orders-table.blade.php
@@ -2,19 +2,19 @@
 <div class="card">
     <div class="card-body">
         <div class="row">
-            <div class="col-lg-2 col-6">
+            <div class="col-lg-2 col-6 mb-2">
                 <div class="fw-600">Order number</div>
                 <div>#{{$order->order_id}}</div>
             </div>
-            <div class="col-lg-2 col-6">
+            <div class="col-lg-2 col-6 mb-2">
                 <div class="fw-600">Date placed</div>
                 <span>{{$order->created_at}}</span>
             </div>
-            <div class="col-lg-2 col-6">
+            <div class="col-lg-2 col-6 mb-2">
                 <div class="fw-600">Items</div>
                 <span>{{$order->items_count}}</span>
             </div>
-            <div class="col-lg-2 col-6">
+            <div class="col-lg-2 col-6 mb-2">
                 <div class="fw-600">Total amount</div>
                 <span>${{$order->total_price}}</span>
             </div>
