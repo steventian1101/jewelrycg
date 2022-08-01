2ef808b223a9422abebc64e04c8315f9a61af714
steventian1101 <steventian1101@gmail.com>
2022-08-01 19:49:54 +0000

    Update index.blade.php

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -16,21 +16,19 @@
         </div>
         
         @can('edit', $user)
-            <div class="d-flex justify-content-end mt-3">
-                <div class="card">
-                    <div class="card-body">
-                        <a href="{{route('user.edit.password')}}" class="btn btn-outline-dark">Change My Password</a>
-                        <a href="{{route('user.edit')}}" class="btn btn-outline-success">Edit my informations</a>
-                        <form action="{{route('user.delete')}}" method="post" class="d-inline">
-                            @csrf
-                            @method('delete')
-                            <button type="submit" class="btn btn-outline-danger">
-                                Delete Account
-                            </button>
-                        </form>
-                    </div>
-                </div>
+        <div class="card">
+            <div class="card-body">
+                <a href="{{route('user.edit.password')}}" class="btn btn-outline-dark">Change My Password</a>
+                <a href="{{route('user.edit')}}" class="btn btn-outline-success">Edit my informations</a>
+                <form action="{{route('user.delete')}}" method="post" class="d-inline">
+                    @csrf
+                    @method('delete')
+                    <button type="submit" class="btn btn-outline-danger">
+                        Delete Account
+                    </button>
+                </form>
             </div>
+        </div>
         @endcan
     </div>
 </div>
