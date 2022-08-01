b02adc807ed419548d04d4f9f5d5d1df0b3ed1ab
steventian1101 <steventian1101@gmail.com>
2022-08-01 07:55:36 +0000

    Update products-table.blade.php

--- resources/views/components/products-table.blade.php
+++ resources/views/components/products-table.blade.php
@@ -1,11 +1,28 @@
-<style>
-    #total_td>div,
-    #shipping_td,
-    #tax_td {
-        font-weight: bold;
-        font-size: medium;
-    }
-</style>
+@foreach ($products as $key => $product)
+    <div class="cart-item mb-3">
+        <div class="row">
+            <div class="col-3">
+                <img src="{{ asset('uploads/all/' . $product->model->uploads->file_name) }}" alt=""
+                    class="thumbnail border rounded w-100">
+            </div>
+            <div class="col-7">
+                <div class="item-meta mb-2">
+                    @php
+                        if (count($product->options)) {
+                            echo $product->name . ' - ' . $product->options->name;
+                        } else {
+                            echo $product->name;
+                        }
+                    @endphp
+                </div>
+                <div class="item-meta mb-2"><span class="fw-800">Quantity:</span> {{ $product->qty }}</div>
+            </div>
+            <div class="col-2 text-right">
+                <span class="text-primary fw-800">${{ $product->price }}</span>
+            </div>
+        </div>
+    </div>
+@endforeach
 
 <table class="table">
     <thead>
