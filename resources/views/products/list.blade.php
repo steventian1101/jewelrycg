6a20a3eb544838e3569e498f81c415ff8b7b5ae7
steventian1101 <steventian1101@gmail.com>
2022-08-01 22:03:10 +0000

    Update list.blade.php

--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -1,15 +1,8 @@
 <x-app-layout page-title="3D Models">
-<section class="hero-home py-9">
-<!--
-    <div class="hero-media" id="hero-media">
-      <video class="hero-media-asset is-visible" autoplay="" muted="" loop="" playsinline="" data-hero-video="" data-src-lg="https://cdn.dribbble.com/uploads/39417/original/49dbf46eae15d227fc95a69cee31251e.mp4?1657824906" data-src-sm="https://cdn.dribbble.com/uploads/39418/original/0cc960a3bf612d0badc4f6165eb36f7b.mp4?1657824915">
-        <source src="https://cdn.dribbble.com/uploads/39417/original/49dbf46eae15d227fc95a69cee31251e.mp4?1657824906" type="video/mp4">
-      </video>
-    </div>
-    -->
+<section class="hero-home pt-9 pb-6">
     <div class="container">
         <div class="row">
-            <div class="col-lg-12 hero-content-container">
+            <div class="col-12 hero-content-container">
                 <div class="hero-categories filter-categories pb-4">
                     <ul class="mb-3">
                     <li class="category active"><a href="#">Explore</a></li>
@@ -19,9 +12,9 @@
                     </ul>
                 </div>
             </div> 
-            <div class="col-lg-6 mx-auto text-center hero-content-container">
-                <h4 class="fs-20 text-white pb-4 mb-0">The world's preferred source for Jewelry CG content</h4>
-                <h1 class="text-white font-weight-bold pb-4 mb-0">Explore our vast collections of 3D models</h1>
+            <div class="col-lg-6 mx-auto hero-content-container">
+                <h4 class="fs-20 pb-4 mb-0">The world's preferred source for Jewelry CG content</h4>
+                <h1 class="font-weight-bold pb-4 mb-0">Explore our vast collections of 3D models</h1>
                 <div class="search-form ml-auto mr-auto py-2">
                     <form method="get" action="{{route('search')}}">
                         <div class="search-col">
