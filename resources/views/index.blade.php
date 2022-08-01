a1856dfe2043b306be2d106613a8dc1c3eda7ada
steventian1101 <steventian1101@gmail.com>
2022-08-01 21:46:50 +0000

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout page-class="homepage" page-title="Jewelry CG">
 
-<section class="hero-home py-9">
+<section class="hero-home pt-10 pb-9">
 <!--
     <div class="hero-media" id="hero-media">
       <video class="hero-media-asset is-visible" autoplay="" muted="" loop="" playsinline="" data-hero-video="" data-src-lg="https://cdn.dribbble.com/uploads/39417/original/49dbf46eae15d227fc95a69cee31251e.mp4?1657824906" data-src-sm="https://cdn.dribbble.com/uploads/39418/original/0cc960a3bf612d0badc4f6165eb36f7b.mp4?1657824915">
