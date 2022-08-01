76841ce6525121a3c04892fc94f331276b3ad4f0
steventian1101 <steventian1101@gmail.com>
2022-08-01 19:55:30 +0000

    Update edit.blade.php

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,27 +1,31 @@
 <x-app-layout page-title="My Informations">
-    <form action="{{route('user.update')}}" method="post">
-        @csrf
-        @method('put')
+<div class="container">
+    <div class="col-xl-4 col-lg-6 col-md-8 py-9 mx-auto">
+        <form action="{{route('user.update')}}" method="post">
+            @csrf
+            @method('put')
 
-        @if ($errors->any())
-            <div class="row justify-content-center mb-3">
-                <div class="card col-6">
-                    <div class="card-body">
-                        @include('includes.validation-form')
+            @if ($errors->any())
+                <div class="row justify-content-center mb-3">
+                    <div class="card col-6">
+                        <div class="card-body">
+                            @include('includes.validation-form')
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endif
+            @endif
 
-        <div class="row m-3">
-            <x-user-info-main :edit="true" :user="auth()->user()"/>
-        </div>
-        <div class="d-flex justify-content-end mt-3">
-            <div class="card">
-                <div class="card-body">
-                    <button type="submit" class="btn btn-outline-primary">Edit my informations</button>
-                </div>
+            <div class="row m-3">
+                <x-user-info-main :edit="true" :user="auth()->user()"/>
             </div>
-        </div>    
-    </form>
+            <div class="d-flex justify-content-end mt-3">
+                <div class="card">
+                    <div class="card-body">
+                        <button type="submit" class="btn btn-outline-primary">Edit my informations</button>
+                    </div>
+                </div>
+            </div>    
+        </form>
+    </div>
+</div>  
 </x-app-layout>
