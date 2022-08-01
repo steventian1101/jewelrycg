24eb68b8306f66d8591475a9d2976d235a96cc2d
steventian1101 <steventian1101@gmail.com>
2022-08-01 19:45:51 +0000

    Update index.blade.php

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,35 +1,37 @@
 <x-app-layout page-title="My Informations">
-<div class="container py-9">
-    @if (session('message'))
-        <div class="row justify-content-center">
-            <div class="card col-md-6 mb-3">
-                <div class="card-body text-success text-center">
-                    {{session('message')}}
+<div class="container">
+    <div class="col-lg-8 col-md-10 py-9 mx-auto checkout-wrap">
+        @if (session('message'))
+            <div class="row justify-content-center">
+                <div class="card col-md-6 mb-3">
+                    <div class="card-body text-success text-center">
+                        {{session('message')}}
+                    </div>
                 </div>
             </div>
+        @endif
+        
+        <div class="row">
+            <x-user-info-main :user="$user" :edit='false'/>
         </div>
-    @endif
-    
-    <div class="row">
-        <x-user-info-main :user="$user" :edit='false'/>
+        
+        @can('edit', $user)
+            <div class="d-flex justify-content-end mt-3">
+                <div class="card">
+                    <div class="card-body">
+                        <a href="{{route('user.edit.password')}}" class="btn btn-outline-dark">Change My Password</a>
+                        <a href="{{route('user.edit')}}" class="btn btn-outline-success">Edit my informations</a>
+                        <form action="{{route('user.delete')}}" method="post" class="d-inline">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="btn btn-outline-danger">
+                                Delete Account
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        @endcan
     </div>
-    
-    @can('edit', $user)
-        <div class="d-flex justify-content-end mt-3">
-            <div class="card">
-                <div class="card-body">
-                    <a href="{{route('user.edit.password')}}" class="btn btn-outline-dark">Change My Password</a>
-                    <a href="{{route('user.edit')}}" class="btn btn-outline-success">Edit my informations</a>
-                    <form action="{{route('user.delete')}}" method="post" class="d-inline">
-                        @csrf
-                        @method('delete')
-                        <button type="submit" class="btn btn-outline-danger">
-                            Delete Account
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    @endcan
 </div>
 </x-app-layout>
