122ec5c36fca736e4b82946eca49f279f3aa8a51
steventian1101 <steventian1101@gmail.com>
2022-08-01 19:54:13 +0000

    Update edit_password.blade.php

--- resources/views/users/edit_password.blade.php
+++ resources/views/users/edit_password.blade.php
@@ -1,27 +1,29 @@
 <x-app-layout page-title="Edit Password">
-<div class="py-9">
-    <form action="{{route('user.update.password')}}" method="post">
-        @csrf
-        @method('patch')
-        <div class="row justify-content-center mt-5">
-            <div class="card">
-                <div class="card-body">
-                    @include('includes.validation-form')
-                    <label for="old_password">Current Password:</label>
-                    <input type="password" name="old_password" id="old_password" class="form-control">
-                    <br>
-                    <label for="new_password">New Password:</label>
-                    <input type="password" name="new_password" id="new_password" class="form-control">
-                    <br>
-                    <label for="new_password_confirmation">Confirm New Password:</label>
-                    <input type="password" name="new_password_confirmation" id="new_password_confirmation" class="form-control">
-                    <br>
-                    <div class="text-center">
-                        <button type="submit" class="btn btn-outline-success">Edit Password</button>
+<div class="container">
+    <div class="col-xl-4 col-lg-6 col-md-8 py-9 mx-auto">
+        <form action="{{route('user.update.password')}}" method="post">
+            @csrf
+            @method('patch')
+            <div class="row justify-content-center mt-5">
+                <div class="card">
+                    <div class="card-body">
+                        @include('includes.validation-form')
+                        <label for="old_password">Current Password:</label>
+                        <input type="password" name="old_password" id="old_password" class="form-control">
+                        <br>
+                        <label for="new_password">New Password:</label>
+                        <input type="password" name="new_password" id="new_password" class="form-control">
+                        <br>
+                        <label for="new_password_confirmation">Confirm New Password:</label>
+                        <input type="password" name="new_password_confirmation" id="new_password_confirmation" class="form-control">
+                        <br>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-outline-success">Edit Password</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </form>
+        </form>
+    </div>
 </div>
 </x-app-layout>
