6b75fb8238327f3d71fe9b8a7544b45fe64150c0
steventian1101 <steventian1101@gmail.com>
2022-08-05 16:49:49 -0700

    fixed the file manager

--- resources/views/backend/file_manager/files-pagination.blade.php
+++ resources/views/backend/file_manager/files-pagination.blade.php
@@ -47,13 +47,13 @@
     @foreach ($files as $file)
     <div class="col-md-3">
         <div class="card p-4 file-manager-item" id="item{{ $file->id }}" data-id="{{ $file->id }}"
-            data-file-path="{{-- $file->getCacheImageFile() --}}">
+            data-file-path="{{ $file->getImageCacheFullName() }}">
             <div class="check-option d-none">✔</div>
             <span class="file-created-at">{{ date('F d, Y, h:i:s A', strtotime($file->created_at)) }}</span>
             @if ($file->type != 'image')
               <img src="{{ asset('assets/svg/brands/google-docs-icon.svg') }}" alt="">
             @else
-              <img src="{{-- $file->getCacheImageFile() --}}" class="card-img-top img-thumbnail" alt="{{ $file->file_name }}">
+              <img src="{{ $file->getImageCacheFullName() }}" class="card-img-top img-thumbnail" alt="{{ $file->file_name }}">
             @endif
             <div class="card-body">
                 <h5 class="card-title text-center">{{ $file->getOriginalFileFullName() }}</h5>
