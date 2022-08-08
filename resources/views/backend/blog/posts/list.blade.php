0dcffb9666c0684bcfd32fed3874c961c7a7d4cd
steventian1101 <steventian1101@gmail.com>
2022-08-08 22:21:36 +0000

    Update list.blade.php

--- resources/views/backend/blog/posts/list.blade.php
+++ resources/views/backend/blog/posts/list.blade.php
@@ -25,76 +25,74 @@
 </div>
 <!-- End Page Header -->
 
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card rounded-0">
-                        <div class="datatable-custom position-relative">
+<div class="row">
+    <div class="col-md-12">
+        <div class="card rounded-0">
+            <div class="datatable-custom position-relative">
 
-                                <table class="table table-lg table-thead-bordered table-nowrap table-align-middle card-table dataTable table-responsive no-footer">
-                                    <thead class="thead-light">
-                                        <th class="sorting_disabled" aria-label="">
-                                            <div class="form-checkbox">
-                                                <input class="form-check-input" type="checkbox" value="" id="datatableCheckAll">
-                                                <label class="form-check-label" for="datatableCheckAll"></label>
-                                            </div>
-                                        </th>
-                                        <th>ID</th>
-                                        <th>Name</th>
-                                        <!--<th>Slug</th>-->
-                                        <th>Author</th>
-                                        <th>Categories</th>
-                                        <th>Actions</th>
-                                    </thead>
+                    <table class="table table-lg table-thead-bordered table-nowrap table-align-middle card-table dataTable table-responsive no-footer">
+                        <thead class="thead-light">
+                            <th class="sorting_disabled" aria-label="">
+                                <div class="form-checkbox">
+                                    <input class="form-check-input" type="checkbox" value="" id="datatableCheckAll">
+                                    <label class="form-check-label" for="datatableCheckAll"></label>
+                                </div>
+                            </th>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <!--<th>Slug</th>-->
+                            <th>Author</th>
+                            <th>Categories</th>
+                            <th>Actions</th>
+                        </thead>
 
-                                    <tbody>
-                                        @foreach ($posts as $post)
-                                        <tr>
-                                        <td class="table-column-pe-0">
-                                            <div class="form-check">
-                                                <input type="checkbox" class="form-check-input" id="ordersCheck1">
-                                                <label class="form-check-label" for="ordersCheck1"></label>
-                                            </div>
-                                        </td>
-                                        <td>{{ $post->id }}</td>
-                                        <td>{{ $post->name }}</td>
-                                        <!--<td>{{ $post->slug }}</td>-->
-                                        <td>{{ $post->postauthor->name }}</td>
-                                        <td>
-                                            @foreach($post->categories as $category_info)
-                                                <p><span class="badge btn-info"> {{$category_info->category->category_name}} </span>  </p>
-                                            @endforeach
-                                        </td>
-                                        <td>
-                                            <div class="btn-group" role="group">
-                                                <a class="btn btn-dark btn-sm" target="_blank" href="#"> <i class="bi-eye"></i> View </a>
-                                                <!-- Button Group -->
-                                                <div class="btn-group">
-                                                    <button type="button" class="btn btn-dark btn-icon btn-sm dropdown-toggle dropdown-toggle-empty" id="ordersExportDropdown1" data-bs-toggle="dropdown" aria-expanded="false"></button>
-                                                    <div class="dropdown-menu dropdown-menu-end mt-1" aria-labelledby="ordersExportDropdown1" style="">
-                                                        <span class="dropdown-header">Options</span>
-                                                        <a href="{{ route('backend.posts.edit', $post->id) }}" class="js-export-print dropdown-item">
-                                                            <i class="bi-pencil-fill me-1"></i> Edit Post
-                                                        </a>
-                                                        <div class="dropdown-divider"></div>
-                                                        <a class="dropdown-item" onclick="return confirm('Are you sure you want to delete this post?')" href="{{route('backend.posts.delete', $post->id)}}"> <i class="bi-trash dropdown-item-icon"></i> Delete </a>
-                                                    </div>
-                                                </div>
-                                                <!-- End Unfold -->
-                                            </div>
-                                            <!-- End Button -->
+                        <tbody>
+                            @foreach ($posts as $post)
+                            <tr>
+                            <td class="table-column-pe-0">
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" id="ordersCheck1">
+                                    <label class="form-check-label" for="ordersCheck1"></label>
+                                </div>
+                            </td>
+                            <td>{{ $post->id }}</td>
+                            <td>{{ $post->name }}</td>
+                            <!--<td>{{ $post->slug }}</td>-->
+                            <td>{{ $post->postauthor->name }}</td>
+                            <td>
+                                @foreach($post->categories as $category_info)
+                                    <p><span class="badge btn-info"> {{$category_info->category->category_name}} </span>  </p>
+                                @endforeach
+                            </td>
+                            <td>
+                                <div class="btn-group" role="group">
+                                    <a class="btn btn-dark btn-sm" target="_blank" href="#">View</a>
+                                    <!-- Button Group -->
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-dark btn-icon btn-sm dropdown-toggle dropdown-toggle-empty" id="ordersExportDropdown1" data-bs-toggle="dropdown" aria-expanded="false"></button>
+                                        <div class="dropdown-menu dropdown-menu-end mt-1" aria-labelledby="ordersExportDropdown1" style="">
+                                            <span class="dropdown-header">Options</span>
+                                            <a href="{{ route('backend.posts.edit', $post->id) }}" class="js-export-print dropdown-item">Edit Post</a>
+                                            <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item text-danger" onclick="return confirm('Are you sure you want to delete this post?')" href="{{route('backend.posts.delete', $post->id)}}">Delete</a>
+                                        </div>
+                                    </div>
+                                    <!-- End Unfold -->
+                                </div>
+                                <!-- End Button -->
 
-                                        </td>
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                         
-                        </div>
-                    </div>
-                </div>
-
-
+                            </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                
             </div>
+        </div>
+    </div>
+
+
+</div>
 
 @endsection
 
