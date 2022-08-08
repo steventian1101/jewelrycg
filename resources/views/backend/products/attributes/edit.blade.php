fb7ea28a9a8df96343c8f0927b8136885f709fec
steventian1101 <steventian1101@gmail.com>
2022-08-08 23:40:14 +0000

    Update edit.blade.php

--- resources/views/backend/products/attributes/edit.blade.php
+++ resources/views/backend/products/attributes/edit.blade.php
@@ -8,56 +8,54 @@
     <!-- End Row -->
 </div>  
 
-            <div class="row">
-                <div class="col-md-12">
-                    <form action="{{route('backend.products.attributes.update', $attribute->id)}}" method="post" enctype="multipart/form-data">
-                        @csrf
-                        @method('PUT')
-                        <div class="row justify-content-center">
-                            <div class="card col-md-12">
-                                <div class="card-body row">
-                                    @include('includes.validation-form')
-                                    <div class="col-md-6 mb-2">
-                                        <label for="name">Name:</label>
-                                        <input value="{{ $attribute->name }}" type="text" name="name" id="name" value="" class="form-control">
-                                    </div>
-                                    <div class="col-md-6 mb-2">
-                                        <label for="name">Type:</label>
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <select class="selectpicker col-md-12" name="type" data-live-search="true">
-                                                    <option value="0"  data-tokens="">Select</option>
-                                                    <option value="1" @if($attribute->type == 1) selected @endif data-tokens="">Color</option>
-                                                    <option value="2" @if($attribute->type == 2) selected @endif data-tokens="">Image</option>
-                                                  </select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12 text-center">
-                                        <button type="submit" class="btn btn-lg btn-outline-success">Update</button>
-                                    </div>
-                                </div>
+<div class="row">
+    <div class="col-md-12">
+        <form action="{{route('backend.products.attributes.update', $attribute->id)}}" method="post" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="card">
+                <div class="card-body">
+                    @include('includes.validation-form')
+                    <div class="col-md-6 mb-2">
+                        <label for="name">Name:</label>
+                        <input value="{{ $attribute->name }}" type="text" name="name" id="name" value="" class="form-control">
+                    </div>
+                    <div class="col-md-6 mb-2">
+                        <label for="name">Type:</label>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <select class="selectpicker col-md-12" name="type" data-live-search="true">
+                                    <option value="0"  data-tokens="">Select</option>
+                                    <option value="1" @if($attribute->type == 1) selected @endif data-tokens="">Color</option>
+                                    <option value="2" @if($attribute->type == 2) selected @endif data-tokens="">Image</option>
+                                    </select>
                             </div>
                         </div>
-                    </form>
+                    </div>
+                    <div class="col-md-12 text-center">
+                        <button type="submit" class="btn btn-lg btn-primary">Update</button>
+                    </div>
                 </div>
             </div>
+        </form>
+    </div>
+</div>
 
-    @endsection
+@endsection
 
-    @section('js_content')
-    
-    <script>
-         $(document).ready(function(){
-            $('#name').keyup(function(){
-                var slug = $(this).val()
-                
-                if(slug.charAt(slug.length - 1) != " ")
-                {
-                    $('#slug').val(slug.replace(/\s+/g, '-').toLowerCase());
-                }
-                
-            })
-         })
-    </script> 
-    @endsection
+@section('js_content')
+
+<script>
+    $(document).ready(function(){
+    $('#name').keyup(function(){
+        var slug = $(this).val()
+        
+        if(slug.charAt(slug.length - 1) != " ")
+        {
+            $('#slug').val(slug.replace(/\s+/g, '-').toLowerCase());
+        }
+        
+    })
+    })
+</script> 
+@endsection
