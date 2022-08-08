f36ce7f205025524dfe5069949c01add9866f0ff
steventian1101 <steventian1101@gmail.com>
2022-08-08 21:33:10 +0000

    Update cart-drawer.blade.php

--- resources/views/products/cart-drawer.blade.php
+++ resources/views/products/cart-drawer.blade.php
@@ -1,69 +1,32 @@
-<style>
-    button.remove-item-btn:after {
-        display: inline-block;
-        content: "\00d7";
-    }
-
-    a.view-cart, a.checkout {
-      margin-top: 8px;
-      text-decoration: none;
-      padding: 8px;
-      border-radius: 2px;
-      display: block;
-      text-align: center;
-      cursor: pointer;
-    }
-
-    a.view-cart {
-      background-color: #eaedf0;
-      color: #393f45;
-    }
-
-    a.view-cart:hover {
-      background-color: #d1d6db;
-      transition: 0.5s;
-    }
-
-    a.checkout {
-      background-color: #282a30;
-      color: #d5d5d6;
-    }
-
-    a.checkout:hover {
-      background-color: #55565a;
-      transition: 0.5s;
-    }
-</style>
-
-    @foreach ($items as $item)
-        @php
-            $product = $item->model;
-        @endphp
-        <div class="row border-bottom" id="item{{ $item->rowId }}">
-            <div class="col-md-3">
-                <img src="{{ $product->uploads->getImageOptimizedFullName() }}" alt="" class="product-thumbnail border w-100">
+@foreach ($items as $item)
+    @php
+        $product = $item->model;
+    @endphp
+    <div class="row border-bottom" id="item{{ $item->rowId }}">
+        <div class="col-4">
+            <img src="{{ $product->uploads->getImageOptimizedFullName() }}" alt="" class="product-thumbnail border w-100">
+        </div>
+        <div class="col-8">
+            <div class="d-flex justify-content-between">
+                <span class="product-title">{{ $product->name }}</span>
+                <button class="remove-item-btn btn btn-danger btn-sm" data-row-id="{{ $item->rowId }}"></button>
             </div>
-            <div class="col-md-9">
-                <div class="d-flex justify-content-between">
-                    <span class="product-title">{{ $product->name }}</span>
-                    <button class="remove-item-btn btn btn-danger btn-sm" data-row-id="{{ $item->rowId }}"></button>
-                </div>
-                <div class="py-4 d-flex justify-content-between">
-                    <input type="number" class="product-quantity p-1" value="{{ $item->qty }}" data-row-id="{{ $item->rowId }}" style="width: 60px;">
-                    <span class="product-price d-none">${{ $product->price / 100 }}</span>
-                    <span class="total-price">${{ $item->qty * $product->price / 100 }}</span>
-                </div>
+            <div class="py-4 d-flex justify-content-between">
+                <input type="number" class="product-quantity p-1" value="{{ $item->qty }}" data-row-id="{{ $item->rowId }}" style="width: 60px;">
+                <span class="product-price d-none">${{ $product->price / 100 }}</span>
+                <span class="total-price">${{ $item->qty * $product->price / 100 }}</span>
             </div>
         </div>
-    @endforeach
-    <div class="row">
-      <div class="col-md-12">
-        <a class="view-cart" href="{{ url('/cart') }}">View Cart</a>
-      </div>
-      <div class="col-md-12">
-        <a class="checkout" href="{{ url('/checkout') }}">Checkout</a>
-      </div>
     </div>
+@endforeach
+<div class="row">
+  <div class="col-md-12">
+    <a class="view-cart" href="{{ url('/cart') }}">View Cart</a>
+  </div>
+  <div class="col-md-12">
+    <a class="checkout" href="{{ url('/checkout') }}">Checkout</a>
+  </div>
+</div>
 
 
 <script>
