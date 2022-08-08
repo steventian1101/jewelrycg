a5e278cdfd742ac7ba30d7701938d8035b84eaf4
steventian1101 <steventian1101@gmail.com>
2022-08-08 22:11:49 +0000

    Update nav.blade.php

--- resources/views/backend/layouts/footer/nav.blade.php
+++ resources/views/backend/layouts/footer/nav.blade.php
@@ -1,36 +1,11 @@
 <!-- Footer -->
 <div class="footer">
-      <div class="row justify-content-between align-items-center">
-        <div class="col">
-          <p class="fs-6 mb-0">© 2022 G.G.M & Co. Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</p>
-        </div>
-        <!-- End Col -->
-
-        <div class="col-auto">
-          <div class="d-flex justify-content-end">
-            <!-- List Separator -->
-            <ul class="list-inline list-separator">
-              <li class="list-inline-item">
-                <a class="list-separator-link" href="#">FAQ</a>
-              </li>
-
-              <li class="list-inline-item">
-                <a class="list-separator-link" href="#">License</a>
-              </li>
-
-              <li class="list-inline-item">
-                <!-- Keyboard Shortcuts Toggle -->
-                <button class="btn btn-ghost-secondary btn btn-icon btn-ghost-secondary rounded-circle" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasKeyboardShortcuts" aria-controls="offcanvasKeyboardShortcuts">
-                  <i class="bi-command"></i>
-                </button>
-                <!-- End Keyboard Shortcuts Toggle -->
-              </li>
-            </ul>
-            <!-- End List Separator -->
-          </div>
-        </div>
-        <!-- End Col -->
-      </div>
-      <!-- End Row -->
-    </div>
+<div class="row">
+<div class="col">
+<p class="fs-6 mb-0">© 2022 G.G.M & Co. Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</p>
+</div>
+<!-- End Col -->
+</div>
+<!-- End Row -->
+</div>
 <!-- End Footer -->
