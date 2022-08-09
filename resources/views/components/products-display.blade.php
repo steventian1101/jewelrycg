cabd7b47be2c80810d80582ba3d78d8abdbca6a9
steventian1101 <steventian1101@gmail.com>
2022-08-09 06:23:30 +0000

    Update products-display.blade.php

--- resources/views/components/products-display.blade.php
+++ resources/views/components/products-display.blade.php
@@ -9,7 +9,7 @@
             <div class="text-left px-2">
                 <div class="row">
                     <div class="fw-700 fs-15 text-primary col-4">${{$product->price}} </div>
-                    <div class="ml-auto col-8 text-black">
+                    <div class="ml-auto col-8 text-black text-right">
                         <span class="fs-12 px-2"><i class="bi bi-heart-fill"></i> 54</span>
                         <span class="fs-12"><i class="bi bi-eye-fill"></i> 434</span>
                     </div>
