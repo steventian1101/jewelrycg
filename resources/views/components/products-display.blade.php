c1d6587b418c7ba09366904281590ab2fa9c0095
steventian1101 <steventian1101@gmail.com>
2022-08-12 03:37:18 +0000

    Update products-display.blade.php

--- resources/views/components/products-display.blade.php
+++ resources/views/components/products-display.blade.php
@@ -1,7 +1,7 @@
-<div class="row row-cols-xxl-5 row-cols-xl-5 row-cols-lg-4 row-cols-md-4 row-cols-2">
+<div class="row row-cols-xxl-6 row-cols-xl-6 row-cols-lg-4 row-cols-md-4 row-cols-2">
 @foreach ($products->chunk(4) as $products_chunk)
     @foreach ($products_chunk as $product)
-    <div class="col mt-1 mb-2">
+    <div class="col mt-1 mb-4">
         <a href="{{route('products.show', $product->slug)}}">
             <div class="card mb-2">
                 <img src="{{ $product->uploads->getImageOptimizedFullName(400) }}" alt="{{ $product->name }}" class="rounded w-100 lazyloaded">
