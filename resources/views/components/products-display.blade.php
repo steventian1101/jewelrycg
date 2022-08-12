113d8e6c9f17f1095f71eeb6181cc7928f39ac05
steventian1101 <steventian1101@gmail.com>
2022-08-12 03:36:34 +0000

    Update products-display.blade.php

--- resources/views/components/products-display.blade.php
+++ resources/views/components/products-display.blade.php
@@ -7,7 +7,7 @@
                 <img src="{{ $product->uploads->getImageOptimizedFullName(400) }}" alt="{{ $product->name }}" class="rounded w-100 lazyloaded">
             </div>
             <div class="text-left px-2">
-                <div class="fw-700 fs-15 text-primary col-4">${{$product->price}} </div>
+                <div class="fw-700 fs-16 text-primary col-4">${{$product->price}} </div>
                 <!--<div class="row align-items-center opacity-70">
                     <div class="fw-700 fs-15 text-primary col-4">${{$product->price}} </div>
                     <div class="ml-auto col-8 text-black text-right">
@@ -16,7 +16,7 @@
                     </div>
                 </div>
                 -->
-                <h3 class="fw-600 fs-14 text-truncate-2 lh-1-4 mb-0 h-35px text-black">
+                <h3 class="fw-600 fs-16 text-truncate-2 lh-1-4 mb-0 h-35px text-black">
                     {{$product->name}}
                 </h3>
             </div>
