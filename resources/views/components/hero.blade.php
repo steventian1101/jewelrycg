b483a286e8af5bc935b3cca4f8be7c894cca8572
steventian1101 <steventian1101@gmail.com>
2022-08-12 18:18:46 +0000

    Update hero.blade.php

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -37,6 +37,36 @@
     </main>
 
     <script>
-    
+    $(function() {
+            var categoryId = '';
+
+            const search = function() {
+                var searchWord = $('#search').val();
+
+                $.ajax({
+                    url: "{{ url('/searchCategory') }}",
+                    data: {
+                        q: searchWord,
+                        category: categoryId
+                    },
+                    success: function(data) {
+                        $('div.product-container').html(data);
+                    }
+                })
+            }
+
+            $('li.category').click(function() {
+                var _this = this;
+                $('ul.category-container').find('li.category').each(function() {
+                    $(this).removeClass('active');
+                    $(_this).addClass('active');
+                });
+
+                categoryId = $(_this).attr('data-category');
+                $('#category_id').val(categoryId);
+
+                search();
+            });
+        })
     </script>
 </div>
