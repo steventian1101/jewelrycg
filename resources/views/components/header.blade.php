279889d1f465013d2897e342397f2ef604f234d1
steventian1101 <steventian1101@gmail.com>
2022-08-13 17:38:05 +0000

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -90,7 +90,7 @@
                         <li><a class="dropdown-item" href="/dashboard">Dashboard</a></li>
                         <li><a class="dropdown-item" href="{{route('user.index', auth()->user()->id)}}">My Info</a></li>
                         <li><a class="dropdown-item" href="{{route('orders.index')}}">{{ auth()->user()->is_admin ? 'All Orders' : 'My Orders' }}</a></li>
-                        <li><a class="dropdown-item" href="{{--route('cart.wishlist')--}}">My Wishlist</a></li>
+                        <li><a class="dropdown-item" href="{{route('wishlist')}}">My Wishlist</a></li>
                         <li><hr class="dropdown-divider"></li>
                         <li><a class="dropdown-item" href="{{ route('logout') }}">Logout</a></li>
                     </ul>
