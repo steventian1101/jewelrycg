0dc681f92c78ede8ea45c5ff773507f03d9a781d
steventian1101 <steventian1101@gmail.com>
2022-08-14 00:26:15 +0000

    Update FormatPrices.php

--- app/Models/Traits/FormatPrices.php
+++ app/Models/Traits/FormatPrices.php
@@ -6,7 +6,7 @@ trait FormatPrices
 {
     public function setPriceToFloat()
     {
-        //$this->price = number_format($this->price / 100, 2);
+        $this->price = number_format($this->price / 100, 2);
         return $this;        
     }
 }
