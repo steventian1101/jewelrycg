106fee8050267d007bccb1f3a5e9330d42106eb5
steventian1101 <steventian1101@gmail.com>
2022-08-14 01:29:03 +0000

    Update user-info-main.blade.php

--- resources/views/components/user-info-main.blade.php
+++ resources/views/components/user-info-main.blade.php
@@ -1,5 +1,6 @@
 <div class="col-12">
     <div class="card">
+        <div class="card-header">User Information</div>
         <div class="card-body">
             @if ($edit)
                 <div class="mb-2">
@@ -35,7 +36,8 @@
     </div>
 </div>
 <div class="col-12">
-    <div class="card">
+    <div class="card mb-4">
+        <div class="card-header">Shipping Address</div>
         <div class="card-body">
             <div class="mb-2">
                 <label for="address1">Address:</label>
@@ -74,5 +76,46 @@
                     placeholder="{{ $user->address->postal_code ?? ''}}" class="form-control">
             </div>
         </div>
-    </div>
+    </div><!-- end shipping-address-->
+    <div class="card">
+        <div class="card-header">Billing Address</div>
+        <div class="card-body">
+            <div class="mb-2">
+                <label for="address1">Address:</label>
+                <input {{ $edit ? null : 'disabled' }} type="text" name="address1" id="address1"
+                    value="{{ old('address1') ?? $user->address->address ?? ''}}"
+                    placeholder="{{ $user->address->address ?? ''}}" class="form-control">
+            </div>
+            <div class="mb-2">
+                <label for="city">City:</label>
+                <input {{ $edit ? null : 'disabled' }} type="text" name="city" id="city"
+                    value="{{ old('city') ?? $user->address->city ?? ''}}" placeholder="{{ $user->address->city ?? ''}}"
+                    class="form-control">
+            </div>
+            <div class="mb-2">
+                <label for="country">Country:</label>
+                <input {{ $edit ? null : 'disabled' }} type="text" name="country" id="country"
+                    value="{{ old('country') ?? $user->address->country ?? ''}}"
+                    placeholder="{{ $user->address->country ?? ''}}" class="form-control">
+            </div>
+            <div class="mb-2">
+                <label for="address2">Secondary Address:</label>
+                <input {{ $edit ? null : 'disabled' }} type="text" name="address2" id="address2"
+                    value="{{ old('address2') ?? $user->address->address2 ?? ''}}"
+                    placeholder="{{ $user->address->address2 ?? ''}}" class="form-control">
+            </div>
+            <div class="mb-2">
+                <label for="state">State:</label>
+                <input {{ $edit ? null : 'disabled' }} type="text" name="state" id="state"
+                    value="{{ old('state') ?? $user->address->state ?? ''}}" placeholder="{{ $user->address->state ?? ''}}"
+                    class="form-control">
+            </div>
+            <div class="mb-2">
+                <label for="pin_code">PIN Code:</label>
+                <input {{ $edit ? null : 'disabled' }} type="text" name="pin_code" id="pin_code"
+                    value="{{ old('pin_code') ?? $user->address->postal_code ?? ''}}"
+                    placeholder="{{ $user->address->postal_code ?? ''}}" class="form-control">
+            </div>
+        </div>
+    </div><!-- end billing-address-->
 </div>
