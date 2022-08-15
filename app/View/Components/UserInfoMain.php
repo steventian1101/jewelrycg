4ec60cb4ed101d95e62497109a1906dfc4cc9fe8
steventian1101 <steventian1101@gmail.com>
2022-08-14 20:41:58 -0400

    ADD: Save Profile Function

--- app/View/Components/UserInfoMain.php
+++ app/View/Components/UserInfoMain.php
@@ -3,12 +3,15 @@
 namespace App\View\Components;
 
 use App\Models\User;
+use App\Models\UserAddress;
 use Illuminate\View\Component;
 
 class UserInfoMain extends Component
 {
-    public function __construct(public bool $edit = false, public User $user)
+
+    public function __construct(public bool $edit = false, public User $user, public UserAddress $shipping, public UserAddress $billing)
     {
+        // $this->shipping_address = $shipping_address;
     }
 
     public function render()
