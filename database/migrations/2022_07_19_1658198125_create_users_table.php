50aa288febdb548ba8983732edfde0abd4b66c5a
steventian1101 <steventian1101@gmail.com>
2022-08-18 17:22:37 +0000

    Update 2022_07_19_1658198125_create_users_table.php

--- database/migrations/2022_07_19_1658198125_create_users_table.php
+++ database/migrations/2022_07_19_1658198125_create_users_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('phone')->nullable();
             $table->string('address_shipping')->nullable();
             $table->string('address_billing')->nullable();
             $table->rememberToken();
