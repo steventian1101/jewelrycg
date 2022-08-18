3a06c7061d4fcb193bbf2c3dec84bddae18387f3
steventian1101 <steventian1101@gmail.com>
2022-08-18 20:04:35 +0000

    Update edit_password.blade.php

--- resources/views/users/edit_password.blade.php
+++ resources/views/users/edit_password.blade.php
@@ -4,23 +4,21 @@
         <form action="{{route('user.update.password')}}" method="post">
             @csrf
             @method('patch')
-            <div class="row justify-content-center mt-5">
-                <div class="card">
-                    <div class="card-header">Change Password</div>
-                    <div class="card-body">
-                        @include('includes.validation-form')
-                        <label for="old_password">Current Password:</label>
-                        <input type="password" name="old_password" id="old_password" class="form-control">
-                        <br>
-                        <label for="new_password">New Password:</label>
-                        <input type="password" name="new_password" id="new_password" class="form-control">
-                        <br>
-                        <label for="new_password_confirmation">Confirm New Password:</label>
-                        <input type="password" name="new_password_confirmation" id="new_password_confirmation" class="form-control">
-                        <br>
-                        <div class="d-block">
-                            <button type="submit" class="btn btn-primary">Edit Password</button>
-                        </div>
+            <div class="card">
+                <div class="card-header">Change Password</div>
+                <div class="card-body">
+                    @include('includes.validation-form')
+                    <label for="old_password">Current Password:</label>
+                    <input type="password" name="old_password" id="old_password" class="form-control">
+                    <br>
+                    <label for="new_password">New Password:</label>
+                    <input type="password" name="new_password" id="new_password" class="form-control">
+                    <br>
+                    <label for="new_password_confirmation">Confirm New Password:</label>
+                    <input type="password" name="new_password_confirmation" id="new_password_confirmation" class="form-control">
+                    <br>
+                    <div class="d-block">
+                        <button type="submit" class="btn btn-primary">Edit Password</button>
                     </div>
                 </div>
             </div>
