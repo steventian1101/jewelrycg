d6e803870cf8f1bbdcde332ced6a4f74d9c322ea
steventian1101 <steventian1101@gmail.com>
2022-08-22 19:57:58 -0500

    add the name of site in title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,26 +6,26 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- SEO start -->
-        <meta name="description" content="{{ $pageTitle}}">
+        <meta name="description" content="{{ $pageTitle}} | Jewelry CG">
 
         <!-- Google / Search Engine Tags -->
-        <meta itemprop="name" content="{{$pageTitle}}">
+        <meta itemprop="name" content="{{$pageTitle}} | Jewelry CG">
         <meta itemprop="description" content="{{ $pageTitle}}">
 
         <!-- Facebook Meta Tags -->
         <meta property="og:type" content="website">
-        <meta property="og:title" content="{{$pageTitle}}">
+        <meta property="og:title" content="{{$pageTitle}} | Jewelry CG">
         <meta property="og:description" content="{{ $pageTitle}}">
 
         <!-- Twitter Meta Tags -->
         <meta name="twitter:card" content="summary_large_image">
-        <meta name="twitter:title" content="{{$pageTitle}}">
+        <meta name="twitter:title" content="{{$pageTitle}} | Jewelry CG">
         <meta name="twitter:description" content="{{ $pageTitle}}">
 
         <!-- SEO end -->
 
 
-        <title>{{$pageTitle}}</title>
+        <title>{{$pageTitle}} | Jewelry CG</title>
         <link rel="shortcut icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}"/>
 
         <!-- Google Fonts -->
