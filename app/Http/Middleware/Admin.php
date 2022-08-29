c31f7caecb7be61034664986672a6c74369216f0
steventian1101 <steventian1101@gmail.com>
2022-08-29 22:14:42 +0000

    Update Admin.php

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -16,7 +16,7 @@ class Admin
      */
     public function handle(Request $request, Closure $next)
     {
-        abort_if(! auth()->user()->is_admin, 401);
+        abort_if(! auth()->user()->role, 401);
 
         return $next($request);
     }
