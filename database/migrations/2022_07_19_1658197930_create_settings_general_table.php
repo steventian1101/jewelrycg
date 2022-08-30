aecc926e8e4b1df8fb71aef65bdf7debbe7c813c
steventian1101 <steventian1101@gmail.com>
2022-08-30 22:11:41 +0000

    Update 2022_07_19_1658197930_create_settings_general_table.php

--- database/migrations/2022_07_19_1658197930_create_settings_general_table.php
+++ database/migrations/2022_07_19_1658197930_create_settings_general_table.php
@@ -15,12 +15,31 @@ return new class extends Migration
     {
         Schema::create('settings_general', function (Blueprint $table) {
             $table->id();
+
+            //social
             $table->string('sitename')->nullable();
             $table->string('twitter')->nullable();
             $table->string('facebook')->nullable();
             $table->string('instagram')->nullable();
             $table->string('youtube')->nullable();
-            $table->timestamps();
+
+            //stripe
+            $table->string('stripe_key')->nullable();
+            $table->string('stripe_secret')->nullable();
+
+            //mail
+            $table->string('mail_mailer')->nullable();
+            $table->string('mail_host')->nullable();
+            $table->string('mail_port')->nullable();
+            $table->string('mail_username')->nullable();
+            $table->string('mail_password')->nullable();
+            $table->string('mail_encryption')->nullable();
+            $table->string('mail_from_address')->nullable();
+            $table->string('mail_from_name')->nullable();
+
+            // recaptcha
+            $table->string('recaptcha_site_key')->nullable();
+            $table->string('recaptcha_secret_key')->nullable();
         });
     }
 
