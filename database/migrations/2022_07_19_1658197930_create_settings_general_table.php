264465f1240e5c514158d66ed8230825649ca86f
steventian1101 <steventian1101@gmail.com>
2022-08-30 22:02:09 +0000

    Update 2022_07_19_1658197930_create_settings_general_table.php

--- database/migrations/2022_07_19_1658197930_create_settings_general_table.php
+++ database/migrations/2022_07_19_1658197930_create_settings_general_table.php
@@ -1,1 +1,36 @@
+<?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings_general', function (Blueprint $table) {
+            $table->id();
+            $table->string('sitename')->nullable();
+            $table->string('twitter')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('youtube')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings_general');
+    }
+};
