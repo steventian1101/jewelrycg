a55a5656f72e6cb8865e3e27e3460c75afda2d55
steventian1101 <steventian1101@gmail.com>
2022-09-01 20:57:32 -0500

    fix bug autoselect country

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -271,8 +271,10 @@ class CheckoutController extends Controller
         $shippings = ShippingOption::all();
         $products = Cart::instance('default')->content();
         $shipping_address = auth()->user()->address_shipping ?  UserAddress::find(auth()->user()->address_shipping) : "NULL";
+        $user_ip = request()->ip();
+        $location = geoip()->getLocation($user_ip);
 
-        return view('checkout.shipping')->with(['countries' => $countries, 'shippings' => $shippings, 'products' => $products, 'locale' => 'checkout','shipping'=> $shipping_address ]);
+        return view('checkout.shipping')->with(['countries' => $countries, 'shippings' => $shippings, 'products' => $products, 'locale' => 'checkout','shipping'=> $shipping_address, 'location'=> $location ]);
     }
 
     public function postShipping(Request $request)
@@ -344,7 +346,7 @@ class CheckoutController extends Controller
 
         $user_ip = request()->ip();
         $location = geoip()->getLocation($user_ip);
-
+        
         return view('checkout.billing')->with(['countries' => $countries, 'products' => $products, 'locale' => 'checkout', 'isIncludeShipping' => $isIncludeShipping, 'billing'=> $billing_address, 'location' => $location ]);
     }
 
