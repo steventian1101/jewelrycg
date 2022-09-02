2cfee47105ee17d9522b2fa802e8d82bf37dbb54
steventian1101 <steventian1101@gmail.com>
2022-09-02 01:06:30 +0000

    Update CheckoutController.php

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -274,7 +274,7 @@ class CheckoutController extends Controller
         $shipping_address = auth()->user()->address_shipping ?  UserAddress::find(auth()->user()->address_shipping) : "NULL";
 
         $user_ip = request()->ip();
-        $location = geoip($user_ip);
+        $location = geoip()->getLocation($user_ip);
 
         return view('checkout.shipping')->with(['countries' => $countries, 'shippings' => $shippings, 'products' => $products, 'locale' => 'checkout','shipping'=> $shipping_address, 'location' => $location ]);
     }
@@ -347,7 +347,7 @@ class CheckoutController extends Controller
         $billing_address = auth()->user()->address_billing ?  UserAddress::find(auth()->user()->address_billing) : "NULL";
 
         $user_ip = request()->ip();
-        $location = geoip($user_ip);
+        $location = geoip()->getLocation($user_ip);
 
         return view('checkout.billing')->with(['countries' => $countries, 'products' => $products, 'locale' => 'checkout', 'isIncludeShipping' => $isIncludeShipping, 'billing'=> $billing_address, 'location' => $location ]);
     }
