677df1465a18d4233f32e3e8d165a67162b435e8
steventian1101 <steventian1101@gmail.com>
2022-09-04 23:26:21 +0000

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -22,6 +22,7 @@ class Order extends Model
         'total',
         'tax_total',
         'grand_total',
+        'shipping_total',
         'tax',
         'shipping_address1',
         'shipping_address2',
