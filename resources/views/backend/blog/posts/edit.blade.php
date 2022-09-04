f21eea5382ac4165ac602658f607d3205a6f2924
steventian1101 <steventian1101@gmail.com>
2022-09-05 02:54:53 +0800

    removed wysiwyg editor

--- resources/views/backend/blog/posts/edit.blade.php
+++ resources/views/backend/blog/posts/edit.blade.php
@@ -182,7 +182,7 @@
     <script>
         $(document).ready(function() {
             $('#desc').trumbowyg();
-            $('#meta_description').trumbowyg();
+            // $('#meta_description').trumbowyg();
         })
         $(".imgAdd").click(function() {
             $(this).closest(".row").find('.imgAdd').before(
