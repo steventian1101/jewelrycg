7139bbc434b587cbb3bd4b50b7261bebc40f1f78
steventian1101 <steventian1101@gmail.com>
2022-09-06 20:27:05 +0000

    Update 2022_07_19_1658197930_create_settings_general_table.php

--- database/migrations/2022_07_19_1658197930_create_settings_general_table.php
+++ database/migrations/2022_07_19_1658197930_create_settings_general_table.php
@@ -33,6 +33,7 @@ return new class extends Migration
             $table->string('mail_from_name')->nullable();
             $table->string('recaptcha_site_key')->nullable();
             $table->string('recaptcha_secret_key')->nullable();
+            $table->tinyInteger('guest_checkout')->nullable();
             $table->timestamps();
         });
     }
