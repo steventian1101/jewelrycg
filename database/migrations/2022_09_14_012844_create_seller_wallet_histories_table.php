0fbcf70d2fd65e0a0cdf4e938a7fcd3ff33a9716
steventian1101 <steventian1101@gmail.com>
2022-09-14 09:48:35 +0800

    added wallet history table, model

---
+++ database/migrations/2022_09_14_012844_create_seller_wallet_histories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seller_wallet_histories', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->integer('amount');
+            $table->string('type')->default('add')->comment('add, withdraw');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seller_wallet_histories');
+    }
+};
