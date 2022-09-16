c79df16c386680fa679440dc37ba79d9d17979ee
steventian1101 <steventian1101@gmail.com>
2022-09-16 19:26:26 +0000

    Update cart.blade.php

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout page-title="Cart">
 <div class="container">
-    <div class="col-lg-8 col-md-10 py-9 mx-auto cart-wrap">
+    <div class="col-lg-4 col-md-6 py-9 mx-auto cart-wrap">
         <div class="card">
             @if (session('message'))
                 <h4 class="text-center text-danger mt-3">
