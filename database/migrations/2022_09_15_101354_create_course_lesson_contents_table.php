40eba1bd87aa29b51ec0694b2470ce245f337fe9
steventian1101 <steventian1101@gmail.com>
2022-09-19 01:23:10 +0000

    Update 2022_09_15_101354_create_course_lesson_contents_table.php

--- database/migrations/2022_09_15_101354_create_course_lesson_contents_table.php
+++ database/migrations/2022_09_15_101354_create_course_lesson_contents_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('course_lesson_contents', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('lesson_id');
+            $table->string('name');
             $table->text('content');
             $table->timestamps();
         });
