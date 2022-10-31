1b23aa0429df3719203c9fc1828857e53ea6f336
steventian1101 <steventian1101@gmail.com>
2022-10-31 19:43:44 +0000

    Update 2022_10_28_092154_create_services.php

--- database/migrations/2022_10_28_092154_create_services.php
+++ database/migrations/2022_10_28_092154_create_services.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
+            $table->integer('status');
             $table->integer('user_id');
             $table->string('name');
             $table->string('slug');
