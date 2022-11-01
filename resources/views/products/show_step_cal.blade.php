a935c2ce306067d8cc7ec0ffc433054334092792
steventian1101 <steventian1101@gmail.com>
2022-11-01 22:56:55 +0000

    Update show_step_cal.blade.php

--- resources/views/products/show_step_cal.blade.php
+++ resources/views/products/show_step_cal.blade.php
@@ -17,146 +17,140 @@ $current_rate = CurrentRate::getLastRate();
 
 <div class="card">
     <div class="card-body">
-        <div class="accordion">
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="">
-                    <button class="accordion-button collapsed" type="button"
-                        data-bs-toggle="collapse" data-bs-target="#"
-                        aria-expanded="true" aria-controls="">
-                        Step1: Select the metal below you want to make this item with.
-                    </button>
-                </h2>
-                <div id="">
-                    <div class="accordion-body">
-                        <div class="row">
-                            @foreach ($arrProductMaterials as $product_material)
-                                @php
-                                    $type_name = $product_material->type;
-                                    $type_id = $product_material->material_type_id;
-                                    $material_weight = $product_material->material_weight;
-                                    $material_dwt = $material_weight * 0.64301;
-                                    $diamond_tamount = 0;
-                                    foreach($arrProductDiamonds as $diamond) {
-                                        $diamond_tamount += $diamond->diamond_amount;
-                                    }
+        <h2 class="accordion-header" id="">
+            <button class="accordion-button collapsed" type="button"
+                data-bs-toggle="collapse" data-bs-target="#"
+                aria-expanded="true" aria-controls="">
+                Step1: Select the metal below you want to make this item with.
+            </button>
+        </h2>
+        <div id="">
+            <div class="accordion-body">
+                <div class="row">
+                    @foreach ($arrProductMaterials as $product_material)
+                        @php
+                            $type_name = $product_material->type;
+                            $type_id = $product_material->material_type_id;
+                            $material_weight = $product_material->material_weight;
+                            $material_dwt = $material_weight * 0.64301;
+                            $diamond_tamount = 0;
+                            foreach($arrProductDiamonds as $diamond) {
+                                $diamond_tamount += $diamond->diamond_amount;
+                            }
 
-                                    if ($current_rate == null) {
-                                        $price = 0;
-                                        $price_change = 0;
-                                    } else {
-                                        if (strpos($type_name ,'24K') !== false) {
-                                            $rate = $current_rate['24k'];
-                                        } else if (strpos($type_name ,'22K') !== false) {
-                                            $rate = $current_rate['22k'];
-                                        } else if (strpos($type_name ,'18K') !== false) {
-                                            $rate = $current_rate['18k'];
-                                        } else if (strpos($type_name ,'14K') !== false) {
-                                            $rate = $current_rate['14k'];
-                                        } else if (strpos($type_name ,'10K') !== false) {
-                                            $rate = $current_rate['10k'];
-                                        }
+                            if ($current_rate == null) {
+                                $price = 0;
+                                $price_change = 0;
+                            } else {
+                                if (strpos($type_name ,'24K') !== false) {
+                                    $rate = $current_rate['24k'];
+                                } else if (strpos($type_name ,'22K') !== false) {
+                                    $rate = $current_rate['22k'];
+                                } else if (strpos($type_name ,'18K') !== false) {
+                                    $rate = $current_rate['18k'];
+                                } else if (strpos($type_name ,'14K') !== false) {
+                                    $rate = $current_rate['14k'];
+                                } else if (strpos($type_name ,'10K') !== false) {
+                                    $rate = $current_rate['10k'];
+                                }
 
-                                        $price = number_format($material_weight * $rate, 2, '.', ',');
-                                    }
-                                @endphp
+                                $price = number_format($material_weight * $rate, 2, '.', ',');
+                            }
+                        @endphp
 
-                                <div class="col-lg-4 col-6">
-                                    <div class="border p-3 item-value-card mb-3 cal-select-item" data-metal_id="{{$type_id}}">
-                                        <div class="item-value-card-body">
-                                            <div class="value-title pb-2 mb-2 text-uppercase fw-700">
-                                                {{ $type_name }}
-                                            </div>
-                                            <div class="py-1">
-                                                <span class="value-price">${{ $price }}</span>
-                                                <input type="hidden" name="metal_price" class="metal_price" value="{{ $price }}" />
-                                                <input type="hidden" name="metal_price_rate" class="metal_price_rate" value="{{ round($rate, 2) }}" />
-                                                <input type="hidden" name="metal_weight" class="metal_weight" value="{{ $material_weight }}" />
-                                            </div>
-                                            <div class="py-1 fw-700 fs-24">{{ $material_weight }} Grams</div>
-                                            <div class="py-1 fw-700 fs-14">{{ $material_dwt }} DWT</div>
-                                        </div>
+                        <div class="col-lg-4 col-6">
+                            <div class="border p-3 item-value-card mb-3 cal-select-item" data-metal_id="{{$type_id}}">
+                                <div class="item-value-card-body">
+                                    <div class="value-title pb-2 mb-2 text-uppercase fw-700">
+                                        {{ $type_name }}
                                     </div>
+                                    <div class="py-1">
+                                        <span class="value-price">${{ $price }}</span>
+                                        <input type="hidden" name="metal_price" class="metal_price" value="{{ $price }}" />
+                                        <input type="hidden" name="metal_price_rate" class="metal_price_rate" value="{{ round($rate, 2) }}" />
+                                        <input type="hidden" name="metal_weight" class="metal_weight" value="{{ $material_weight }}" />
+                                    </div>
+                                    <div class="py-1 fw-700 fs-24">{{ $material_weight }} Grams</div>
+                                    <div class="py-1 fw-700 fs-14">{{ $material_dwt }} DWT</div>
                                 </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+        <h2 class="accordion-header" id="">
+            <button class="accordion-button collapsed" type="button"
+                data-bs-toggle="collapse" data-bs-target="#"
+                aria-expanded="true" aria-controls="">
+                Step2: Select the type of diamond you want to below.
+            </button>
+        </h2>
+        <div>
+            <div class="accordion-body">
+                <div class="row">                        
+                    <div class="col-lg-6 col-6">
+                        <div class="border p-3 item-value-card mb-3 diamondtype-select-item" data-diamondtype_id="1">
+                            <div class="item-value-card-body">
+                                <div class="py-1 fw-700 fs-24">Natural Diamonds</div>
+                                <div class="py-1 fw-700 fs-14">Natural Diamonds</div>
+                            </div>
+                        </div>
+                    </div>                        
+                    <div class="col-lg-6 col-6">
+                        <div class="border p-3 item-value-card mb-3 diamondtype-select-item" data-diamondtype_id="2">
+                            <div class="item-value-card-body">
+                                <div class="py-1 fw-700 fs-24">Lab Diamonds</div>
+                                <div class="py-1 fw-700 fs-14">Lab Diamonds</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="accordion-body">
+            <div class="table-full-width">
+                <div class="col-md-12">
+                    <table id="product_total_estimate_price" class="table table-lg table-bordered table-nowrap table-align-middle card-table dataTable table-responsive no-footer">
+                        <thead>
+                            <th>Estimated Cost</th>
+                            <th id="total_estimate_price"></th>
+                        </thead>
+                        <tbody>
+                            <tr class="metal_price">
+                                <td class="metal_price_category"></td>
+                                <td class="metal_total_price"></td>
+                            </tr>
+                            
+                            @foreach ($arrProductDiamonds as $diamond)
+                                <tr class="natural_price">
+                                    <td class="product_diamond_category">{{ $diamond->mm_size }} mm ({{$diamond->tcw}} * ${{ $diamond->natural_price }})</td>
+                                    <td class="product_diamond_price">${{ ($diamond->tcw * $diamond->natural_price) }}</td>
+                                </tr>
                             @endforeach
-                        </div>
-                    </div>
-                </div>
-                <h2 class="accordion-header" id="">
-                    <button class="accordion-button collapsed" type="button"
-                        data-bs-toggle="collapse" data-bs-target="#"
-                        aria-expanded="true" aria-controls="">
-                        Step2: Select the type of diamond you want to below.
-                    </button>
-                </h2>
-                <div>
-                    <div class="accordion-body">
-                        <div class="row">                        
-                            <div class="col-lg-6 col-6">
-                                <div class="border p-3 item-value-card mb-3 diamondtype-select-item" data-diamondtype_id="1">
-                                    <div class="item-value-card-body">
-                                        <div class="py-1 fw-700 fs-24">Natural Diamonds</div>
-                                        <div class="py-1 fw-700 fs-14">Natural Diamonds</div>
-                                    </div>
-                                </div>
-                            </div>                        
-                            <div class="col-lg-6 col-6">
-                                <div class="border p-3 item-value-card mb-3 diamondtype-select-item" data-diamondtype_id="2">
-                                    <div class="item-value-card-body">
-                                        <div class="py-1 fw-700 fs-24">Lab Diamonds</div>
-                                        <div class="py-1 fw-700 fs-14">Lab Diamonds</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="accordion-body">
-                    <div class="card strpied-tabled-with-hover rounded-0">
-                        <div class="table-full-width">
-                            <div class="col-md-12">
-                                <table id="product_total_estimate_price" class="table table-lg table-thead-bordered table-nowrap table-align-middle card-table dataTable table-responsive no-footer">
-                                    <thead>
-                                        <th>Estimated Cost</th>
-                                        <th id="total_estimate_price"></th>
-                                    </thead>
-                                    <tbody>
-                                        <tr class="metal_price">
-                                            <td class="metal_price_category"></td>
-                                            <td class="metal_total_price"></td>
-                                        </tr>
-                                        
-                                        @foreach ($arrProductDiamonds as $diamond)
-                                            <tr class="natural_price">
-                                                <td class="product_diamond_category">{{ $diamond->mm_size }} mm ({{$diamond->tcw}} * ${{ $diamond->natural_price }})</td>
-                                                <td class="product_diamond_price">${{ ($diamond->tcw * $diamond->natural_price) }}</td>
-                                            </tr>
-                                        @endforeach
-                                        @foreach ($arrProductDiamonds as $diamond)
-                                            <tr class="lab_price">
-                                                <td class="product_diamond_category">{{ $diamond->mm_size }} mm ({{$diamond->tcw}} * ${{ $diamond->lab_price }})</td>
-                                                <td class="product_diamond_price">${{ ($diamond->tcw * $diamond->lab_price) }}</td>
-                                            </tr>
-                                        @endforeach
-                                        <tr class="printing_cost">
-                                            <td class="printing_cost_title">3D Printing Cost</td>
-                                            <td class="printing_cost_amount"></td>
-                                        </tr>
-                                        @if($diamond_tamount > 0)
-                                        <tr class="diamond_setting_cost">
-                                            <td class="diamond_setting_cost_title">Diamond Setting Cost ({{ $diamond_tamount }} x $1.5)</td>
-                                            <td class="diamond_setting_cost_amount">${{ round($diamond_tamount * 1.5, 2) }}</td>
-                                        </tr>
-                                        @endif
-                                        <tr class="casting_cost">
-                                            <td>Casting Cost</td>
-                                            <td class="casting_cost_amount"></td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
+                            @foreach ($arrProductDiamonds as $diamond)
+                                <tr class="lab_price">
+                                    <td class="product_diamond_category">{{ $diamond->mm_size }} mm ({{$diamond->tcw}} * ${{ $diamond->lab_price }})</td>
+                                    <td class="product_diamond_price">${{ ($diamond->tcw * $diamond->lab_price) }}</td>
+                                </tr>
+                            @endforeach
+                            <tr class="printing_cost">
+                                <td class="printing_cost_title">3D Printing Cost</td>
+                                <td class="printing_cost_amount"></td>
+                            </tr>
+                            @if($diamond_tamount > 0)
+                            <tr class="diamond_setting_cost">
+                                <td class="diamond_setting_cost_title">Diamond Setting Cost ({{ $diamond_tamount }} x $1.5)</td>
+                                <td class="diamond_setting_cost_amount">${{ round($diamond_tamount * 1.5, 2) }}</td>
+                            </tr>
+                            @endif
+                            <tr class="casting_cost">
+                                <td>Casting Cost</td>
+                                <td class="casting_cost_amount"></td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
