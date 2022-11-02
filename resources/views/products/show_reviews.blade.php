8430987f27f6e6d1e653a3ebd763d99a3525b4e9
steventian1101 <steventian1101@gmail.com>
2022-11-02 11:59:41 +0000

    Update show_reviews.blade.php

--- resources/views/products/show_reviews.blade.php
+++ resources/views/products/show_reviews.blade.php
@@ -2,51 +2,6 @@
     {!! $arrReviewListing->links() !!}
 </div>
 
-<style>
-.star-ratings {
-    unicode-bidi: bidi-override;
-    color: #ccc;
-    font-size: 30px;
-    position: relative;
-    margin: 0;
-    padding: 0;
-}
-
-.star-ratings .fill-ratings {
-    color: #387dff;
-    padding: 0;
-    position: absolute;
-    z-index: 1;
-    display: block;
-    top: 0;
-    left: 0;
-    overflow: hidden;
-}
-
-.star-ratings .fill-ratings span {
-    display: inline-block;
-}
-
-.star-ratings .empty-ratings {
-    padding: 0;
-    display: block;
-    z-index: 0;
-}
-
-
-
-.rate-item .reviewer_name {
-    font-size: 16px;
-    font-weight: bold;
-}
-
-.rate-item .rated_date {
-    margin: 4px 0 0 12px !important;
-}
-</style>
-
-
-
 @foreach ($arrReviewListing as $review)
     <div class="user-review-item pb-2 mb-4">
         <div class="row">
@@ -57,7 +12,7 @@
                         <div class="fs-20 fw-600 reviewer_name w-100">{{ $review->user->first_name }} {{ $review->user->last_name }}</div>
                         <div class="row">
                             <div class="col-auto">
-                                <div class="star-ratings">
+                                <div class="star-ratings star-ratings-sm">
                                     <div class="fill-ratings" style="width: {{ $review->rating * 100 / 5 }}%;">
                                         <span>★★★★★</span>
                                     </div>
