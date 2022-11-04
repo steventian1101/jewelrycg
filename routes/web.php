83c9573d0707ebca35d54824549533dad9ae32c2
steventian1101 <steventian1101@gmail.com>
2022-11-04 13:17:37 -0700

    url fix

--- routes/web.php
+++ routes/web.php
@@ -389,7 +389,7 @@ Route::group(['middleware' => ['auth']], function () {
 });
 
 //services
-Route::group(['prefix' => 'service'], function () {
+Route::group(['prefix' => 'services'], function () {
     Route::get('/', [ServicesController::class, 'all'])->name('all');
     Route::get('/{id}', [ServicesController::class, 'detail'])->name('detail');
 });
