a24b4abab783119f006341fead052af4d7299d14
steventian1101 <steventian1101@gmail.com>
2022-11-06 14:00:43 +0000

    Update 2022_11_04_184458_create_services_reviews.php

--- database/migrations/2022_11_04_184458_create_service_reviews.php
+++ database/migrations/2022_11_04_184458_create_service_reviews.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('services_reviews', function (Blueprint $table) {
+        Schema::create('service_reviews', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('service_id');
             $table->unsignedBigInteger('package_id');
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('services_reviews');
+        Schema::dropIfExists('service_reviews');
     }
 };
