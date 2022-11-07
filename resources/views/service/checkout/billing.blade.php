c49e976e416df8124eb17aa0157b30f54445e569
steventian1101 <steventian1101@gmail.com>
2022-11-07 04:43:49 -0800

    service link bugfix

--- resources/views/service/checkout/billing.blade.php
+++ resources/views/service/checkout/billing.blade.php
@@ -7,7 +7,7 @@
                         <div class="logo py-4 fw-800 fs-24">#JEWELRYCG</div>
                         <nav class="pb-4" aria-label="breadcrumb">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="/services/{{ $package->id }}">Service</a></li>
+                                <li class="breadcrumb-item"><a href="/services/{{ $package->service->id }}">Service</a></li>
                                 <li class="breadcrumb-item active" aria-current="page">Billing</li>
                                 <li class="breadcrumb-item" aria-current="page">Payment</li>
                             </ol>
