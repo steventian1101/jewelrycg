014c9295a01c6f979604380a18bfe902b643e1f4
steventian1101 <steventian1101@gmail.com>
2022-11-08 11:44:40 -0800

    added softdelete to service and service package tabel

--- database/migrations/2022_10_28_092442_create_service_packages.php
+++ database/migrations/2022_10_28_092442_create_service_packages.php
@@ -22,6 +22,8 @@ return new class extends Migration
             $table->string('revisions')->nullable();
             $table->integer('delivery_time');
             $table->timestamps();
+
+            $table->softDeletes();
         });
     }
 
