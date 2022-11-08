43c9561c80995660175f3974ea4de1fd88fadcfc
steventian1101 <steventian1101@gmail.com>
2022-11-08 20:38:26 +0000

    Update 2022_11_04_184051_create_orders_services.php

--- database/migrations/2022_11_04_184051_create_orders_services.php
+++ database/migrations/2022_11_04_184051_create_orders_services.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('orders_services', function (Blueprint $table) {
             $table->id();
-            $table->integer('status')->default(0); // 0-pending, 1-revision, 2-canceled, 3-(physical:delivered, digital:shipped)
+            $table->integer('status')->default(0); // 0-pending, 1 = in progress, 2-revision, 3-canceled, 4-(physical:delivered)
             $table->string('order_id');
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('service_id');
