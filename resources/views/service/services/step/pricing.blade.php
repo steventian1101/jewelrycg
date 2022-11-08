282ef4f4f25db6a667ab7eff56866b7f2e566741
steventian1101 <steventian1101@gmail.com>
2022-11-08 14:13:45 -0800

    deleted package show on pricing page

--- resources/views/service/services/step/pricing.blade.php
+++ resources/views/service/services/step/pricing.blade.php
@@ -123,19 +123,19 @@
                             <div class="package-title">BASIC</div>
                             <input type="hidden" name="type[]" value="1"/>
                             <div class="d-flex sub-content">
-                                <textarea type="text" name="name[]" id="name" rows="4" maxlength="35" class="w-100" placeholder="Name your package">{{isset($data->packages) ? (count($data->packages) >= 1 ? $data->packages[0]->name : "") : "" }}</textarea>
+                                <textarea type="text" name="name[]" id="name" rows="4" maxlength="35" class="w-100" placeholder="Name your package">{{isset($packages) ? (count($packages) >= 1 ? $packages[0]->name : "") : "" }}</textarea>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
                             </div>
                             <div class="d-flex sub-content">
-                                <textarea type="text" name="description[]" id="description" rows="6" maxlength="35" class="w-100" placeholder="Describe the details of your offering">{{isset($data->packages) ? (count($data->packages) >= 1 ? $data->packages[0]->description : "") : "" }}</textarea>
+                                <textarea type="text" name="description[]" id="description" rows="6" maxlength="35" class="w-100" placeholder="Describe the details of your offering">{{isset($packages) ? (count($packages) >= 1 ? $packages[0]->description : "") : "" }}</textarea>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
                             </div>
                             <div class="d-flex sub-content">
-                                @php ($value = isset($data->packages) ? (count($data->packages) >= 1 ? $data->packages[0]->delivery_time : 0) : 0)
+                                @php ($value = isset($packages) ? (count($packages) >= 1 ? $packages[0]->delivery_time : 0) : 0)
                                 <select class="form-select select-none" name="delivery_time[]" aria-label="Default select example">
                                     <option {{$value === 0 ? "selected": ""}}>Delivery Time</option>
                                     <option value="1" {{$value === 1 ? "selected": ""}}>1 day Delivery</option>
@@ -158,7 +158,7 @@
                                 </select>
                             </div>
                             <div class="d-flex sub-content">
-                                @php ($value = isset($data->packages) ? (count($data->packages) >= 1 ? $data->packages[0]->revisions : -1) : -1)
+                                @php ($value = isset($packages) ? (count($packages) >= 1 ? $packages[0]->revisions : -1) : -1)
                                 <select type="text" name="revisions[]" class="form-select select-none" placeholder="Revision of your package">
                                     <option {{$value === -1 ? "selected": ""}}>Select</option>
                                     <option value="0" {{$value === "0" ? "selected": ""}}>0</option>
@@ -179,19 +179,19 @@
                             <div class="package-title">STANDARD</div>
                             <input type="hidden" name="type[]" value="2"/>
                             <div class="d-flex sub-content">
-                                <textarea type="text" name="name[]" id="name" rows="4" maxlength="35" class="w-100" placeholder="Name your package">{{isset($data->packages) ? (count($data->packages) >= 2 ? $data->packages[1]->name : "") : "" }}</textarea>
+                                <textarea type="text" name="name[]" id="name" rows="4" maxlength="35" class="w-100" placeholder="Name your package">{{isset($packages) ? (count($packages) >= 2 ? $packages[1]->name : "") : "" }}</textarea>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
                             </div>
                             <div class="d-flex sub-content">
-                                <textarea type="text" name="description[]" id="description" rows="6" maxlength="35" class="w-100" placeholder="Describe the details of your offering">{{isset($data->packages) ? (count($data->packages) >= 2 ? $data->packages[1]->description : "") : "" }}</textarea>
+                                <textarea type="text" name="description[]" id="description" rows="6" maxlength="35" class="w-100" placeholder="Describe the details of your offering">{{isset($packages) ? (count($packages) >= 2 ? $packages[1]->description : "") : "" }}</textarea>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
                             </div>
                             <div class="d-flex sub-content">
-                                @php ($value = isset($data->packages) ? (count($data->packages) >= 2 ? $data->packages[1]->delivery_time : 0) : 0)
+                                @php ($value = isset($packages) ? (count($packages) >= 2 ? $packages[1]->delivery_time : 0) : 0)
                                 <select class="form-select select-none" name="delivery_time[]" aria-label="Default select example">
                                     <option {{$value === 0 ? "selected": ""}}>Delivery Time</option>
                                     <option value="1" {{$value === 1 ? "selected": ""}}>1 day Delivery</option>
@@ -214,7 +214,7 @@
                                 </select>
                             </div>
                             <div class="d-flex sub-content">
-                                @php ($value = isset($data->packages) ? (count($data->packages) >= 2 ? $data->packages[1]->revisions : -1) : -1)
+                                @php ($value = isset($packages) ? (count($packages) >= 2 ? $packages[1]->revisions : -1) : -1)
                                 <select type="text" name="revisions[]" class="form-select select-none" placeholder="Revision of your package">
                                     <option {{$value === -1 ? "selected": ""}}>Select</option>
                                     <option value="0" {{$value === "0" ? "selected": ""}}>0</option>
@@ -235,19 +235,19 @@
                             <div class="package-title">PREMIUM</div>
                             <input type="hidden" name="type[]" value="3"/>
                             <div class="d-flex sub-content">
-                                <textarea type="text" name="name[]" id="name" rows="4" maxlength="35" class="w-100" placeholder="Name your package">{{isset($data->packages) ? (count($data->packages) >= 3 ? $data->packages[2]->name : "") : "" }}</textarea>
+                                <textarea type="text" name="name[]" id="name" rows="4" maxlength="35" class="w-100" placeholder="Name your package">{{isset($packages) ? (count($packages) >= 3 ? $packages[2]->name : "") : "" }}</textarea>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
                             </div>
                             <div class="d-flex sub-content">
-                                <textarea type="text" name="description[]" id="description" rows="6" maxlength="35" class="w-100" placeholder="Describe the details of your offering">{{isset($data->packages) ? (count($data->packages) >= 3 ? $data->packages[2]->description : "") : "" }}</textarea>
+                                <textarea type="text" name="description[]" id="description" rows="6" maxlength="35" class="w-100" placeholder="Describe the details of your offering">{{isset($packages) ? (count($packages) >= 3 ? $packages[2]->description : "") : "" }}</textarea>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
                             </div>
                             <div class="d-flex sub-content">
-                                @php ($value = isset($data->packages) ? (count($data->packages) >= 3 ? $data->packages[2]->delivery_time : 0) : 0)
+                                @php ($value = isset($packages) ? (count($packages) >= 3 ? $packages[2]->delivery_time : 0) : 0)
                                 <select class="form-select select-none" name="delivery_time[]" aria-label="Default select example">
                                     <option {{$value === 0 ? "selected": ""}}>Delivery Time</option>
                                     <option value="1" {{$value === 1 ? "selected": ""}}>1 day Delivery</option>
@@ -270,7 +270,7 @@
                                 </select>
                             </div>
                             <div class="d-flex sub-content">
-                                @php ($value = isset($data->packages) ? (count($data->packages) >= 3 ? $data->packages[2]->revisions : -1) : -1)
+                                @php ($value = isset($packages) ? (count($packages) >= 3 ? $packages[2]->revisions : -1) : -1)
                                 <select type="text" name="revisions[]" class="form-select select-none" placeholder="Revision of your package">
                                     <option {{$value === -1 ? "selected": ""}}>Select</option>
                                     <option value="0" {{$value === "0" ? "selected": ""}}>0</option>
@@ -292,17 +292,17 @@
                         <div class="submission">Price ($)</div>
                         <div class="w-30">
                             <div class="d-flex sub-content">
-                                <input type="number" name="price[]" class="w-100 text-input-package" placeholder="Price of your package" value="{{isset($data->packages) ? (count($data->packages) >= 1 ? $data->packages[0]->price : 0) : 0 }}">
+                                <input type="number" name="price[]" class="w-100 text-input-package" placeholder="Price of your package" value="{{isset($packages) ? (count($packages) >= 1 ? $packages[0]->price : 0) : 0 }}">
                             </div>
                         </div>
                         <div class="w-30">
                             <div class="d-flex sub-content">
-                                <input type="number" name="price[]" class="w-100 text-input-package" placeholder="Price of your package" value="{{isset($data->packages) ? (count($data->packages) >= 2 ? $data->packages[1]->price : 0) : 0 }}">
+                                <input type="number" name="price[]" class="w-100 text-input-package" placeholder="Price of your package" value="{{isset($packages) ? (count($packages) >= 2 ? $packages[1]->price : 0) : 0 }}">
                             </div>
                         </div>
                         <div class="w-30">
                             <div class="d-flex sub-content">
-                                <input type="number" name="price[]" class="w-100 text-input-package" placeholder="Price of your package" value="{{isset($data->packages) ? (count($data->packages) >= 3 ? $data->packages[2]->price : 0) : 0 }}">
+                                <input type="number" name="price[]" class="w-100 text-input-package" placeholder="Price of your package" value="{{isset($packages) ? (count($packages) >= 3 ? $packages[2]->price : 0) : 0 }}">
                             </div>
                         </div>
                     </div>
@@ -346,8 +346,6 @@
             $('#package_count').val(1)
         }
     })
-
-    {!! count($data->packages) >= 3 ? "setofferpk();" : "" !!}
   })
   function setofferpk() {
     $('.toggle-class').bootstrapToggle('on')
