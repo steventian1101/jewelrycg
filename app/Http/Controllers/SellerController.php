90f9b12c6670e855ca64a88f1deb70be850b23be
steventian1101 <steventian1101@gmail.com>
2022-11-09 14:40:02 -0800

    bugfix on seller dashboard

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -36,7 +36,7 @@ class SellerController extends Controller
             }
         }
         $products = Product::where('vendor', auth()->id())->get();
-        $seller = SellersProfile::where('user_id', auth()->id())->first();
+        $seller = SellersProfile::findOrFail(auth()->id());
         $pendingBalance = SellersWalletHistory::where('user_id', auth()->id())->where('status', 0)->select('amount')->get()->sum('amount');
         $totalEarned = SellersWalletHistory::where('user_id', auth()->id())->select('amount')->get()->sum('amount');
         return view('seller.dashboard')->with([
