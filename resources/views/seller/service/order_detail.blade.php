159a30d4c980be23a1eec93fbd67dbd788ef43e3
steventian1101 <steventian1101@gmail.com>
2022-11-09 14:02:05 -0800

    count down on order detail page

--- resources/views/seller/service/order_detail.blade.php
+++ resources/views/seller/service/order_detail.blade.php
@@ -6,7 +6,11 @@
                 <div class="card mb-4">
                     <div class="card-body">
                         <h4 class="fw-700">Order Started</h4>
+                        @if ($order->status == 0)
+                        <p class="p-0">Pending requirements in order to start job. Contact to <b>{{ $order->user->first_name . " " . $order->user->last_name }}</b> and let them know to submit the requirements.</p>
+                        @else
                         <p class="p-0"><b>{{ $order->user->first_name . " " . $order->user->last_name }}</b> sent all the information you need so you can start working on this order. You got this!</p>
+                        @endif
                     </div>
                 </div>
                 <div class="card mb-4">
@@ -15,18 +19,49 @@
                             <i class="bi bi-clipboard-check p-1"></i>
                             <span class=""><b>{{ $order->user->first_name . " " . $order->user->last_name }}</b> placed the order {{ date('F d, Y', strtotime($order->created_at)) }}</span>
                         </div>
+                        @if ($order->status != 0)
                         <div class="timeline-item pt-2 pb-4 border-bottom">
                             <i class="bi bi-clipboard-check p-1"></i>
                             <span class=""><b>{{ $order->user->first_name . " " . $order->user->last_name }}</b> sent the requirements {{ date('F d, Y', strtotime($order->created_at)) }}</span>
                         </div>
+                        @endif
                     </div>
                 </div>
             </div>
             <div class="col-3">
                 <div class="card mb-4 time-left">
                     <div class="card-body">
-                        <h4 class="fw-700">{{ date('F d, Y h:i A', strtotime($order->original_delivery_time)) }}</h4>
+                        @if ($order->status != 0)
+                        <div class="col-md-12" id="count_title">
+                          Time left to deliver
+                        </div>
+                        <div class="col-md-12 d-flex justify-content-between align-items-center my-2">
+                          <div class="d-flex flex-column align-items-center" style="width: 23%;">
+                            <h5 id="count_day">00</h5>
+                            <p class="opacity-70 mb-0">Days</p>
+                          </div>
+                          <div class="bg-black opacity-70" style="width: 1px; height: 30px;"></div>
+                          <div class="d-flex flex-column align-items-center" style="width: 23%;">
+                            <h5 id="count_hour">00</h5>
+                            <p class="opacity-70 mb-0">Hours</p>
+                          </div>
+                          <div class="bg-black opacity-70" style="width: 1px; height: 30px;"></div>
+                          <div class="d-flex flex-column align-items-center" style="width: 23%;">
+                            <h5 id="count_min">00</h5>
+                            <p class="opacity-70 mb-0">Minutes</p>
+                          </div>
+                          <div class="bg-black opacity-70" style="width: 1px; height: 30px;"></div>
+                          <div class="d-flex flex-column align-items-center" style="width: 23%;">
+                            <h5 id="count_sec">00</h5>
+                            <p class="opacity-70 mb-0">Seconds</p>
+                          </div>
+                        </div>
                         <a class="btn btn-primary" href="#">Deliver Now</a>
+                        @else
+                        <div class="col-md-12">
+                          Didn't receive requirement yet
+                        </div>
+                        @endif
                     </div>
                 </div>
                 <div class="card mb-4 order-details">
@@ -48,69 +83,43 @@
                     </div>
                 </div>
             </div>
-
-            <div class="row">
-          <h1 class="fw-800 mb-3">Thanks for shopping with us!</h1>
-          <p>We appreciate your order, we’re currently processing it. So hang tight, and we’ll send you confirmation
-            very soon!</p>
-          <div class="order-items-card border-bottom py-4 mb-5">
-              <div class="row">
-                  <div class="col-lg-3 col-6 mb-2">
-                      <div class="w-100 fs-18 fw-600">Order number</div>
-                      <div class="fs-14 text-primary">#{{ $order->order_id }}</div>
-                  </div>
-                  <div class="col-lg-3 col-6 mb-2">
-                      <div class="w-100 fs-18 fw-600">Payment status</div>
-                      <div class="fs-14 ">
-                          {{ ucwords(Config::get('constants.oder_payment_status')[$order->status_payment]) }}</div>
-                  </div>
-                  <div class="col-lg-3 col-6 mb-2">
-                      <div class="w-100 fs-18 fw-600">Date created</div>
-                      <div class="fs-14 ">{{ date('F d, Y', strtotime($order->created_at)) }}</div>
-                  </div>
-                
-                  @if ($order->status)
-                  <div class="col-lg-3 col-6 mb-2">
-                      <div class="w-100 fs-18 fw-600">Est Deliver Date</div>
-                      <div class="fs-14 ">{{ date('F d, Y h:i A', strtotime($order->original_delivery_time)) }}</div>
-                  </div>
-                  @endif
-              </div>
-          </div>
-
-          <div class="order-items-card pb-4">
-              <div class="row">
-                  <div class="col-lg-2 col-3">
-                      <img src="{{ $order->service->thumb->getImageOptimizedFullName(150) }}" alt=""
-                          class="thumbnail border w-100">
-                  </div>
-                  <div class="col-lg-10 col-9">
-                      <div class="order-item-title fs-24 py-2 fw-600">
-                          {{ $order->service->name }}
-                      </div>
-                      <div class="order-item-qty-price fs-16 pb-2">
-                          <span class="fw-600">Price:</span>
-                          ${{ number_format($order->package_price, 2) }}
-                      </div>
-                      <div class="order-item-qty-price fs-16 pb-2">
-                        <span class="fw-600">Buyer Name:</span>
-                        {{ $order->user->first_name . " " . $order->user->last_name }}
-                      </div>
-                      <div class="is_downloadable fw-600 fs-16">
-                          <div class="d-flex mt-2">
-                                <span class="d-block" data-item-id="{{ $order->id }}">
-                                    {{ 'Status: ' . Config::get('constants.service_order_status')[$order->status] }}
-                                </span>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-
-        </div><!-- end .row-->
+        </div>
     </div>
 </div>
 @section('js')
 <script>
+  var countDownDate = new Date("{{ $order->original_delivery_time }}").getTime()
+
+  function padLeadingZeros(num, size) {
+    if (num <= 0) return "00";
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+  }
+  var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    $('#count_day').text(padLeadingZeros(days));
+    $('#count_hour').text(padLeadingZeros(hours));
+    $('#count_min').text(padLeadingZeros(minutes));
+    $('#count_sec').text(padLeadingZeros(seconds));
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      $('#count_title').text("Delivery time has already passed");
+    }
+  }, 1000);
 </script>
 @endsection
 
