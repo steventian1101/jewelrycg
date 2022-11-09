acf73f57ac1e7c98817e340c034617b738d050f2
steventian1101 <steventian1101@gmail.com>
2022-11-09 10:17:23 -0800

    bugfix on fileanme

--- database/migrations/2022_11_09_130341_create_order_service_deliveries_table.php
+++ database/migrations/2022_11_09_130341_create_order_service_deliveries_table.php
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('order_service_delivers');
+        Schema::dropIfExists('order_service_deliveries');
     }
 };
