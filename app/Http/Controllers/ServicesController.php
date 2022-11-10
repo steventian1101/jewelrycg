2ec2b45b87c729fdd634f5648dd762aa24adc252
steventian1101 <steventian1101@gmail.com>
2022-11-10 14:57:18 -0800

    redirect when detail

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -720,13 +720,15 @@ class ServicesController extends Controller
         $order->payment_intent = $request->get('payment_intent');
         $order->save();
 
-        $requirements = ServiceRequirement::with('choices')->where('service_id', $order->service_id)->get();
+        // $requirements = ServiceRequirement::with('choices')->where('service_id', $order->service_id)->get();
 
         // Mail::to(auth()->user()->email)->send(new OrderPlacedMail($order));
 
         // $request->session()->forget('order_id');
         // redirect to order details
-        return view('service.checkout.order', ['order' => $order, 'requirements' => $requirements]);
+        // return view('service.checkout.order', ['order' => $order, 'requirements' => $requirements]);
+
+        return redirect()->route('service.order_detail', ['id' => $order->order_id]);
     }
 
     public function cancel(CancelCheckoutRequest $req)
