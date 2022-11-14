49cb3cd56771eebfe7a89bb2d3b068c00119ee5c
steventian1101 <steventian1101@gmail.com>
2022-11-13 18:12:18 -0800

    removed slash

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -383,12 +383,10 @@ class CheckoutController extends Controller
             }
         }
         // Send order placed email to customer
-        if (false) {
-            if (auth()->user()) {
-                Mail::to(auth()->user()->email)->send(new OrderPlacedMail($order));
-            } else {
-                Mail::to($request->session()->get('billing_email'))->send(new OrderPlacedMail($order));
-            }
+        if (auth()->user()) {
+            Mail::to(auth()->user()->email)->send(new OrderPlacedMail($order));
+        } else {
+            Mail::to($request->session()->get('billing_email'))->send(new OrderPlacedMail($order));
         }
 
         $request->session()->forget('order_id');
